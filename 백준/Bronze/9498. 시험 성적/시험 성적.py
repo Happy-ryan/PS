aa4d90ad12b647695e8e7733cdedfdf0595a746b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-03-15 11:06:27 +0900

    [Bronze IV] Title: 시험 성적, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if  100>= score >= 90 :
+    print('A')
+elif score >= 80 :
+    print('B')
+elif score >= 70 :
+    print('C')
+elif score >= 60 :
+    print('D')
+elif score >= 0 :
+    print('F')
