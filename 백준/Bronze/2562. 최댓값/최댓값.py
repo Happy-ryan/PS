b6480c9e81920e4634470ed316c53131cd114f55
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-03-19 13:51:51 +0900

    [Bronze II] Title: 최댓값, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,14 +1,8 @@
-N1 = int(input())
-N2 = int(input())
-N3 = int(input())
-N4 = int(input())
-N5 = int(input())
-N6 = int(input())
-N7 = int(input())
-N8 = int(input())
-N9 = int(input())
-list = [N1, N2, N3, N4, N5, N6, N7, N8, N9]
-max_num = max(list)
-line_num = list.index(max_num) + 1
+arr=[]
+for i in range(9) :
+    x = int(input())
+    arr.append(x)
+max_num = max(arr)
+line_num = arr.index(max(arr)) + 1
 print(max_num)
 print(line_num)
