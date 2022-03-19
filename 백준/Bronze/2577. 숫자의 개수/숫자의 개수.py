b725cb383f1c677dcda218d3d4e667b0cd4eb023
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-03-19 14:15:27 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -2,15 +2,7 @@ A = int(input())
 B = int(input())
 C = int(input())
 
-D = A*B*C
+D = str(A*B*C)
 
-print(str(D).count('0'))
-print(str(D).count('1'))
-print(str(D).count('2'))
-print(str(D).count('3'))
-print(str(D).count('4'))
-print(str(D).count('5'))
-print(str(D).count('6'))
-print(str(D).count('7'))
-print(str(D).count('8'))
-print(str(D).count('9'))
+for i in range(10) :
+    print(D.count(str(i)))
