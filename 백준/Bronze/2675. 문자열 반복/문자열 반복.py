9e897a2dd16c68da0868216d5ad21630b7d5a570
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-03-23 18:20:23 +0900

    [Bronze II] Title: 문자열 반복, Time: 64 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,10 +1,7 @@
 T = int(input())
-sum = ""
 for _ in range(T) :
     arr = input()
-    sum=""
     for j in range(2, len(arr)) :
         result = arr[j]*int(arr[0])
-        sum = sum + result
-    print(sum)
-        
+        print(result, end="")
+    print('')
