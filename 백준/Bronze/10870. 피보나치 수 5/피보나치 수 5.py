0ed292fd8e4a15c1879243712b2bb02efd898683
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-04-03 02:11:47 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 116 ms, Memory: 126364 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+def fibonaci(n) :
+    if n == 0 :
+        return 0
+    elif n==1 or n==2 :
+        return 1
+    else :
+        return fibonaci(n-1) + fibonaci(n-2)
+
+print(fibonaci(n))
