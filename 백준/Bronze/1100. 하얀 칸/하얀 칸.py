a0a226ac30fd85b8c1803a4ddd81fefd22fb452f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-04-11 00:01:40 +0900

    [Bronze II] Title: 하얀 칸, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,35 +1,12 @@
 cnt = 0
-s0 = input()
-s1 = input()
-s2 = input()
-s3 = input()
-s4 = input()
-s5 = input()
-s6 = input()
-s7 = input()
-
-for i in [s0[0],s0[2],s0[4],s0[6]] :
-    if i == "F" :
-        cnt +=1
-for i in [s2[0],s2[2],s2[4],s2[6]] :
-    if i == "F" :
-        cnt +=1
-for i in [s4[0],s4[2],s4[4],s4[6]] :
-    if i == "F" :
-        cnt +=1
-for i in [s6[0],s6[2],s6[4],s6[6]] :
-    if i == "F" :
-        cnt +=1
-for i in [s1[1],s1[3],s1[5],s1[7]] :
-    if i == "F" :
-        cnt +=1
-for i in [s3[1],s3[3],s3[5],s3[7]] :
-    if i == "F" :
-        cnt +=1
-for i in [s5[1],s5[3],s5[5],s5[7]] :
-    if i == "F" :
-        cnt +=1
-for i in [s7[1],s7[3],s7[5],s7[7]] :
-    if i == "F" :
-        cnt +=1
+for i in range(8) :
+    s = input()
+    if i%2 == 0 :
+        for j in range(0,7,2) :
+            if s[j] =="F" :
+                cnt += 1
+    elif i%2 != 0 :
+        for k in range(1,8,2) :
+            if s[k] == "F" :
+                cnt += 1
 print(cnt)
