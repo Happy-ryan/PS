385a756459cb89ea3215b806a456fbb9e6f49143
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-04-14 12:52:08 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 112 ms, Memory: 115416 KB -BaekjoonHub

--- 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.py
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.py
@@ -1,7 +1,8 @@
 N = int(input())
 arr=list(map(int, input().split()))
 cnt = 0
-for i in range(1, N+1):
-    if arr[i-1] != i :
+brr = sorted(arr)
+for i in range(N) :
+    if arr[i] != brr[i] :
         cnt +=1
 print(cnt)
