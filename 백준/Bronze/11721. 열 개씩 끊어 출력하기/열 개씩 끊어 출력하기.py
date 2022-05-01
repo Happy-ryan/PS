2793610639e0a3f660e537c6410aba29623c8083
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-05-01 10:56:16 +0900

    [Bronze II] Title: 열 개씩 끊어 출력하기, Time: 104 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -1,16 +1,3 @@
 s = input()
-arr= list(s)
-#print(len(arr))
-arr_list = [ None for _ in range(114)]    
-for i in range(len(arr)) :
-    arr_list[i] = arr[i]
-#print(arr_list)
-for x in range(11) :
-    result = []
-    for y in range(10*x, 10*x+10) :
-        if arr_list[y] != None :
-            result.append(arr_list[y])
-    if result == [] :
-        break    
-    else :
-        print(*result,sep="") # sep="" 공백제거출력
+for i in range(0, len(s), 10):
+    print(s[i:i+10])
