604ba83753360862615ff9568390c788007f6909
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-05-09 00:20:17 +0900

    [Silver III] Title: 문자열 집합, Time: 328 ms, Memory: 126356 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,6 +1,6 @@
 import sys
 N, M = map(int, sys.stdin.readline().split())
-S = set([input() for _ in range(N)]) # list함수를 set으로 하는 이유 : (N) > (logN) 시간단축
+S = set(list(input() for _ in range(N))) # list함수를 set으로 하는 이유 : (N) > (logN) 시간단축
 check_str = list(input() for _ in range(M))
 cnt = 0
 for i in check_str :
