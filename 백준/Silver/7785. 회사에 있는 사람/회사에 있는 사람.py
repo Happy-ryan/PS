0879ae0e29c27407fdb60dc46bdf4c30a4bbd47f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-05-17 18:21:38 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 284 ms, Memory: 134412 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,11 +1,10 @@
 n = int(input())
-arr=[]
+arr=set()
 for _ in range(n):
     s = input().split()
     if s[1]=="enter":
-        arr.append(s[0])
+        arr.add(s[0]) #set함수에서 특정요소 추가하기
     else: # s[1]=="leave" 인 경우
-        idx = arr.index(s[0])
-        arr.pop(idx)
+        arr.remove(s[0]) #set함수에서 특정요소 제거하기. 인덱스를 파악안해도 되기 때문에 훨씬 빠르다.
 arr = sorted(arr)[::-1]
 print(*arr, sep="\n")
