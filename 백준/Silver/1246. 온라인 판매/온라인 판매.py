eb2da99665f76001975dbd367be99e24d496b625
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-05-20 22:33:22 +0900

    [Silver IV] Title: 온라인 판매, Time: 184 ms, Memory: 177180 KB -BaekjoonHub

--- 백준/Silver/1246. 온라인 판매/온라인 판매.py
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.py
@@ -1,15 +1,15 @@
-# 계란이 1개, 사고싶은 사람이 1000명이다.
 N, M = map(int, input().split())
 price = [int(input()) for _ in range(M)]
 price = sorted(price)
 max_price = max(price)
 sum_price=[]
 for num in reversed(range(1,max_price+1)): # 1e6
-    cnt = 0
-    for x in price: # 1000 -> 1e9
-        if num <= x:
-            cnt +=1
-    ret = min(N,cnt) 
+    # for x in price: # 1000 -> 1e9
+    #     if num <= x:
+    #         cnt +=1 ## 하고싶은것: price 안에 x보다 크거나 같은 것의 개수
+    while len(price) and num <= price[-1]:
+        price.pop()
+    ret = min(N, M - len(price))
     sum_price.append(num*ret)
 sum_price.reverse()
 result = max(sum_price)
