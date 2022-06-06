b5933f897b2cef8f7e8377655955052b8621448b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-06-06 22:21:33 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.py
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.py
@@ -1,17 +1,16 @@
-a,b = map(int, input().split())
-
-def f(x) : #이진수x를 십진수로 변환하는 함수
+a, b = input().split()
+def f(a):
     sum = 0
-    for i in range(len(str(x))):
-        sum += int(str(x)[i])*(2**(len(str(x))-1-i))
+    for x in a:
+        sum *= 2
+        sum += int(x)
     return sum
-result = str(f(a)+f(b)) #type int
-if result =='0':
-    print(0)
-else:
-  ans = ''
-  result = int(result)
-  while result:
-      ans += str(result % 2)
-      result //= 2
-  print(ans[::-1])
+c = f(a) + f(b)
+ans = ''
+if c == 0:
+	ans = '0'
+else :
+	while c:
+	    ans += str(c % 2)
+	    c //= 2
+print(ans[::-1])
