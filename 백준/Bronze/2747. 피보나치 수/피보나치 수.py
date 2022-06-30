4323261f87457df41f0809d49b8d1e4842666f11
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-06-30 17:59:35 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,13 +1,11 @@
 #탑다운 다이나믹 프로그래밍 소스코드 - 피보나치수열
 dp = [0]*100 #계산된 결과를 메모이제이션하기 위한 리스트 초기화
-#재귀함수로 구현한 피보나치 + 탑다운
-def fibo(x):
-    if x == 1 or x==2:  #종료조건
-        return 1
-    if dp[x] != 0: #0이 아니라는 것은 이미 계산된 값이 리스트에 저장되었다는 의미
-        return dp[x]
-    dp[x] = fibo(x-1) + fibo(x-2)
-    return dp[x]
+#재귀함수로 구현한 피보나치 + 바텀업 - 반복문사용
+dp[1] = 1
+dp[2] = 1
+n = int(input())
 
-N = int(input())
-print(fibo(N))
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
