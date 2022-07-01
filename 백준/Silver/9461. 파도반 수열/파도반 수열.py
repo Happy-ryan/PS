20122bb82e62e8a58633f04ba45cca4b8a6494e8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-07-01 22:05:17 +0900

    [Silver III] Title: 파도반 수열, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,13 +1,13 @@
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 2
+for i in range(6, 101):
+    dp[i] = dp[i-1] + dp[i-5]
+
 T = int(input())
 for row in range(T):
     n = int(input())
-    dp = [0] * 101
-    dp[1] = 1
-    dp[2] = 1
-    dp[3] = 1
-    dp[4] = 2
-    dp[5] = 2
-    for i in range(6,n+1):
-        dp[i] = dp[i-1] + dp[i-5]
-
     print(dp[n])
