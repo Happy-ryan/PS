17b4d519fa1fcc763f501913c184e748968ec8e1
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-07-02 23:10:46 +0900

    [Silver V] Title: 거스름돈, Time: 116 ms, Memory: 122224 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,17 @@
+n = int(input())
+dp = [0] * 1000006
+dp[1] = -1
+dp[2] = 1
+dp[3] = -1
+dp[4] = 2
+dp[5] = 1
+dp[6] = 3
+dp[7] = 2
+dp[8] = 4
+dp[9] = 3
+dp[10] = 2
+for i in range(11,1000001):
+    dp[i] = min(1+dp[i-5],1+dp[i-2])
+    if dp[i] == 0:
+        print(-1)
+print(dp[n])
