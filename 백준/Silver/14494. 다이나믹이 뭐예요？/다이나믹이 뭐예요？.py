1705a5f25afe4c0c454b32c47bfe05a5628035bb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-07-02 17:32:58 +0900

    [Silver II] Title: 다이나믹이 뭐예요?, Time: 132 ms, Memory: 121524 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -16,6 +16,6 @@ for j in range(1,1003):
     dp[1][j] = 1
 for i in range(2,n+1):
     for j in range(2,m+1):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j-1] + dp[i-1][j]
+        dp[i][j] = (dp[i][j-1] + dp[i-1][j-1] + dp[i-1][j])%1000000007
 
-print(dp[n][m]%1000000007)
+print(dp[n][m])
