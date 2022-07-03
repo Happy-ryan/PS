c6da4e8f658bd5c07c150c4f02337e2cc02c109d
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-07-03 17:48:44 +0900

    [Bronze III] Title: 점수계산, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2506. 점수계산/점수계산.py
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -1,15 +1,12 @@
 n = int(input())
 arr = list(input().split())
-# print(arr)
-sum = ""
 score = 0
+hap = 0
 for x in arr:
-    sum +=x
-sum = sum.split('0')
-# print(sum)
-for row in sum:
-    if '1' in row:
-        for x in range(1,len(row)+1):
-            score +=x
+    if x == '1':
+        hap +=1
+        score += hap
+    else:
+        hap = 0 # 1이 아니면 hap 초기화
 
 print(score)
