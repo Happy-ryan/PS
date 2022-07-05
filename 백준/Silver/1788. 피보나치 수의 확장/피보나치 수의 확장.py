5fcdfa0847623f88d9f4cbec65bef0bf9fcc5971
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-07-05 14:57:58 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 124 ms, Memory: 129960 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -16,12 +16,12 @@ elif n >0:
 else:
     dp_minus[0] = 0 #dp[0]
     dp_minus[1] = 1 #dp[-1]의미
-    dp_minus[2] = -1
+    dp_minus[2] = -1 #dp[-2]의미
     for i in range(3,(-n)+1):
         dp_minus[i] = dp_minus[i-2] - dp_minus[i-1]
         if dp_minus[i] > 0:
             dp_minus[i] = (dp_minus[i])%mod
-        else:
+        else: #((-정수 %  mod ) 하면 원하는 나머지가 나오지 않으므로 양수로 바꾸고 나서 -부호 붙이는 방법을 사용한다.
             dp_minus[i] = -(abs(dp_minus[i])%mod)
     if dp_minus[-n] > 0:
         print(1)
