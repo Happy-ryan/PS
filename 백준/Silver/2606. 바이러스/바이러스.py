c59c8d520b2cdd1b87e318ba6e3ffd0729758334
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-08-08 17:52:37 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -7,19 +7,18 @@ for _ in range(m):
     adj[b].append(a)
 # print(adj)
 
-visited = [False]*(n+1)
-cnt = 0
+visited = [0]*(n+1)
+
 def dfs(cur):
-    global cnt
+    # global cnt
     # 현재 위치 방문 : 문제상 cur는 노드1번에서 시작
     if visited[cur]: return
-    visited[cur] = True
-    cnt += 1
+    visited[cur] = 1 # 방문하면 1
     # 현재 cur에 연결된노드들 nxt 
     # nxt 노드가 방문한 적이 있다면 True로 return 종료
     # nxt 노드가 방문한 적이 없다면 visited[nxt] = True
     for nxt in adj[cur]:
         dfs(nxt)
-    return cnt-1
+    return sum(visited)-1
 
 print(dfs(1))
