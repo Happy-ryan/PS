7f4d74de50657b9d74c4f8f882d4b00bbb2a7c58
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-08-08 17:28:25 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+n = int(input())
+m = int(input())
+adj = [[] for row in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+# print(adj)
+
+visited = [False]*(n+1)
+cnt = 0
+def dfs(cur):
+    global cnt
+    # 현재 위치 방문 : 문제상 cur는 노드1번에서 시작
+    if visited[cur]: return
+    visited[cur] = True
+    cnt += 1
+    # 현재 cur에 연결된노드들 nxt 
+    # nxt 노드가 방문한 적이 있다면 True로 return 종료
+    # nxt 노드가 방문한 적이 없다면 visited[nxt] = True
+    for nxt in adj[cur]:
+        dfs(nxt)
+    return cnt-1
+
+print(dfs(1))
