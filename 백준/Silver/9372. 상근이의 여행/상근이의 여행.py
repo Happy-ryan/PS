0ba6dda4228a54b774d5b9cb9a080b0b32a9febe
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-08-15 22:30:01 +0900

    [Silver IV] Title: 상근이의 여행, Time: 312 ms, Memory: 120064 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -8,20 +8,21 @@ for _ in range(T):
         adj[a].append(b)
         adj[b].append(a)
     
-    result = []
+    # result = []
 
-    for cur in range(1,N+1):
-        in_queue = [False]*(N+1)
-        q = deque([cur])
-        in_queue[cur] = True
-        cnt = 0
-        while q:
-            v = q.popleft()
-            for nxt in adj[v]:
-                if not in_queue[nxt]:
-                    q.append(nxt)
-                    in_queue[nxt] = True
-            cnt += 1
-        result.append(cnt)
+    # for cur in range(1,N+1):
+    #     in_queue = [False]*(N+1)
+    #     q = deque([cur])
+    #     in_queue[cur] = True
+    #     cnt = 0
+    #     while q:
+    #         v = q.popleft()
+    #         for nxt in adj[v]:
+    #             if not in_queue[nxt]:
+    #                 q.append(nxt)
+    #                 in_queue[nxt] = True
+    #         cnt += 1
+    #     result.append(cnt)
 
-    print(cnt-1)
+    # print(cnt-1)
+    print(N - 1)
