a59068f3a35625e3c6d10155f3923a76091e4a99
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-08-15 16:21:57 +0900

    [Silver IV] Title: 상근이의 여행, Time: 1412 ms, Memory: 123364 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,27 @@
+from collections import deque
+T = int(input())
+for _ in range(T):
+    N,M = map(int,input().split())
+    adj =[[] for row in range(N+1)]
+    for _ in range(M):
+        a,b = map(int,input().split())
+        adj[a].append(b)
+        adj[b].append(a)
+    
+    result = []
+
+    for cur in range(1,N+1):
+        in_queue = [False]*(N+1)
+        q = deque([cur])
+        in_queue[cur] = True
+        cnt = 0
+        while q:
+            v = q.popleft()
+            for nxt in adj[v]:
+                if not in_queue[nxt]:
+                    q.append(nxt)
+                    in_queue[nxt] = True
+            cnt += 1
+        result.append(cnt)
+
+    print(cnt-1)
