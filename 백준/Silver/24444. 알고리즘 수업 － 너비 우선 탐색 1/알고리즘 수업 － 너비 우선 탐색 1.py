68e65c0b1973047347b9a60523689bf9c546af84
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-08-23 00:01:36 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 672 ms, Memory: 155760 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,35 @@
+n,m,r = map(int,input().split())
+adj = [[] for row in range(n+1)]
+for i in range(1,m+1):
+    a,b = map(int,input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+
+# print(adj)
+
+for i in range(1,n+1):
+    adj[i] = sorted(adj[i])
+
+# print(adj)
+
+in_queue = [False] *(n+1)
+
+from collections import deque
+q = deque([r])
+in_queue[r] = True
+result = [0]
+while q:
+    cur = q.popleft()
+    result.append(cur)
+    for nxt in adj[cur]:
+        if not in_queue[nxt]:
+            q.append(nxt)
+            in_queue[nxt] = True
+
+# print(result)
+ans=[0]*(n+1)
+# print('-----------------------')
+for i, x in enumerate(result):
+    ans[x] = i
+for k in ans[1:]:
+    print(k)
