23b8efc60e56beae3cb2d2e3fec46311a1dc09e4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-09-05 19:45:07 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 116540 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,14 +4,18 @@ from heapq import heappop, heappush
 input = sys.stdin.readline
 
 N,M,X = map(int,input().split())
-adj = [[] for row in range(N+1)]
+adj = [[] for row in range(N+1)] # X > i 돌아갈 때
+radj = [[] for row in range(N+1)] # i > X 도착할 때 역방향으로 생각 i < X
+
 for _ in range(M):
     a,b,c = map(int,input().split())
     adj[a].append((b,c)) # (도착점, 비용)
+    radj[b].append((a,c)) 
+    
 
 inf = int(1e18)
 
-def dijstrak(S,E):
+def dijstrak(S):
     dist = [inf for col in range(N+1)]
 
     heap = []
@@ -28,10 +32,31 @@ def dijstrak(S,E):
                 dist[nxt] = d+cost
                 heappush(heap,(dist[nxt],nxt))
 
-    return dist[E]
+    return dist
 
-result = []
+def rdijstrak(S):
+    rdist = [inf for col in range(N+1)]
+    
+    heap = []
+    heappush(heap,(0,S))
+    rdist[S] = 0
+
+    while heap:
+        d,cur = heappop(heap)
+
+        if rdist[cur] != d: continue
+
+        for nxt,cost in radj[cur]:
+            if rdist[nxt] > d+cost:
+                rdist[nxt] = d+cost
+                heappush(heap,(rdist[nxt],nxt))
+
+    return rdist
+
+arr = dijstrak(X)
+brr = rdijstrak(X)
+result = 0
 for i in range(1,N+1):
-    result.append(dijstrak(i,X)+dijstrak(X,i))
+    result = max(result,arr[i]+brr[i])
 
-print(max(result))
+print(result)
