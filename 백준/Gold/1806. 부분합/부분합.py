cb419a404b64938270ae8f0fadb3e8210e205da1
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-09-16 23:35:48 +0900

    [Gold IV] Title: 부분합, Time: 152 ms, Memory: 128152 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,27 +1,22 @@
-
+# https://www.acmicpc.net/problem/1806
 import sys
 
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
-data = list(map(int,input().split()))
+N, M = map(int, input().split())
+data = list(map(int, input().split()))
 
 cnt = 0
 sum = 0
 e = 0
-ans = []
+ans = int(1e9)
 
 for s in range(N):
-  while sum < M and e < N:
-    sum += data[e]
-    e += 1
-  if sum >= M:
-    cnt += 1
-    sub = e - s
-    ans.append(sub)
-  sum -= data[s]
-
-if cnt == 0:
-  print(0)
-else:
-  print(min(ans))
+    while e < N and sum < M:
+        sum += data[e]
+        e += 1
+    if sum >= M:
+        ans = min(ans, e - s)
+    sum -= data[s]
+if ans == int(1e9): print(0)
+else: print(ans)
