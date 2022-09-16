ce5fdf6847b147de58f9e8a09dadfeda26569a00
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-09-16 23:31:54 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 135180 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,26 +1,27 @@
+# https://www.acmicpc.net/problem/1929
+
 import sys
 
 input = sys.stdin.readline
 
+
 # 에라토스테네스 체 - 함수
 def f(n):
-  a = [False,False]+[True]*(n-1) # [숫자0,1-소수아님]+[인덱스2~n-소수라고 가정]
-  primes = []
+    a = [False, False] + [True] * (n - 1)  # [숫자0,1-소수아님]+[인덱스2~n-소수라고 가정]
+    primes = []
 
-  for i in range(2,n+1):
-    if a[i]:
-      primes.append(i)
-      for j in range(2*i,n+1,i):
-        a[j] = False
+    for i in range(2, n + 1):
+        if a[i]:
+            primes.append(i)
+            for j in range(2 * i, n + 1, i):
+                a[j] = False
 
-  return primes
+    return primes
 
-N,M = map(int,input().split())
 
-# print(f(N))
-# print(f(M))
-primes_set = set(f(N)+f(M))
-# print(primes_set)
+M, N = map(int, input().split())
+
+primes_set = f(N)
 for x in primes_set:
-    if x >= N and x <= M:
+    if M <= x <= N:
         print(x)
