5db48d200061479d7695ad51c86df5b69812fd05
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-12-23 17:32:12 +0900

    [level 2] Title: 3 x n 타일링, Time: 475.44 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -1,18 +1,14 @@
 def solution(n):
     answer = 0
-    mod = 1000000007
+    mod = 100000007
     dp = [0] * (n + 1)
-    dp[0] = 1
     dp[2] = 3
+    dp[4] = 11
     # n이 홀수인 경우 직사각형의 면적 3*n으로 홀수가 나온다.
-    # 각 나무토막의 넓이는 2이므로 절대로 가로가 홀수인 경우에는 전체를 채울 수 없다.
-    for k in range(4, n+1, 2):
-        for x in range(2, k+1, 2):
-            if x == 2:
-                dp[k] += 3 * dp[k - x]
-            else:
-                dp[k] += 2 * dp[k - x]
-            dp[k] = (dp[k]) % mod
+    # 각 나무토막의 넓이는 2이므로 절대로 가로가 홀수인 경우에는 전체를 채울         수 없다.
+    for k in range(6, n+1, 2):
+        dp[k] = (3 * dp[k - 2] + 2 * dp[k - 4])%mod 
+    
     answer = dp[n]
     
     return answer
--- 프로그래머스/lv2/12902. 3 x n 타일링/README.md
+++ 프로그래머스/lv2/12902. 3 x n 타일링/README.md
@@ -1,6 +1,6 @@
 # [level 2] 3 x n 타일링 - 12902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902#) 
 
 ### 성능 요약
 
