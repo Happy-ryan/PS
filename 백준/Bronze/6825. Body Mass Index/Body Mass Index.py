896be998b879d49ca52c6c9b3629b550bdbc977a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-12-25 22:26:46 +0900

    [Bronze IV] Title: Body Mass Index, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.py
@@ -0,0 +1,10 @@
+weight = float(input())
+height = float(input())
+bmi = weight / (height**2)
+
+if bmi > 25:
+    print('Overweight')
+elif 18.5 <= bmi <= 25:
+    print('Normal weight')
+else:
+    print('Underweight')
