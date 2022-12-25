eef727cd58935e813445f6ce876bdca34a5fd6fc
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2022-12-25 22:35:20 +0900

    [Bronze IV] Title: Squares, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.py
@@ -0,0 +1,9 @@
+import math
+
+N = int(input())
+
+
+if int(math.sqrt(N)) == math.sqrt(N):
+    print(f"The largest square has side length {int(math.sqrt(N))}.")
+else:
+    print(f"The largest square has side length {int(math.sqrt(N))}.")
