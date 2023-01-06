0130c391d961274002a9f2c6e3405ab186bfe5be
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-06 20:31:33 +0900

    [level 2] Title: 소수 찾기, Time: 14.22 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,6 +1,18 @@
-from itertools import permutations
+def dfs(lev, numbers, d, used, visited, final): # 순열, 중복사용 불가
+    if lev == d:
+        a = int(''.join(visited.copy()))
+        final.add(a)
+        return
+    for i in range(len(numbers)):
+        if used[i] == 0:
+            used[i] = 1
+            visited.append(numbers[i])
+            dfs(lev + 1, numbers, d, used, visited, final)
+            used[i] = 0
+            visited.pop()
+    return final
 
-def f(n):
+def isPrime(n):
     primes = []
     a = [False, False] + [True] * (n - 1)
     
@@ -9,14 +21,17 @@ def f(n):
             primes.append(i)
             for j in range(i*i, n + 1, i):
                 a[j] = False
+    
     return primes
 
 def solution(numbers):
     answer = 0
-    num_set = set()
-    for l in range(1, len(numbers) + 1):
-        for row in list(permutations(numbers, l)):
-            num_set.add(int(''.join(row)))
-    for x in f(max(num_set)):
-        if x in num_set: answer += 1
+    used = [0] * len(numbers)
+    visited = []
+    final = set()
+    for i in range(1, len(numbers) + 1):
+        dfs(0, numbers, i, used, visited, final)
+    for x in isPrime(max(final)):
+        if x in final:
+            answer += 1
     return answer
