330335abfb6f834091f688597371e840c825601e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-12 21:05:30 +0900

    [level 2] Title: 전화번호 목록, Time: 1816.45 ms, Memory: 262 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+def solution(phone_book):
+    answer = True
+    prefix_dict = Counter()
+    for row in phone_book:
+        for i in range(len(row)):
+            prefix_dict[row[0: len(row) - i]] += 1
+            
+    for qs in phone_book:
+        if prefix_dict[qs] > 1:
+            answer = False
+    return answer
