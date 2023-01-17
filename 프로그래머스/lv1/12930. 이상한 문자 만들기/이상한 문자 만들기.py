2ac1b7a7a71612747feb3f3b54b9bdcfec5e3725
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-17 13:36:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,22 +1,15 @@
 def solution(s):
     answer = ''
-    # print(list(s))
-    res = [0]
-    for i in range(len(s) - 1):
-        if s[i] == ' ' and s[i + 1] != ' ':
-            res.append(i + 1)
-    res.append(len(s))
-    # print(res)
-    for i in range(len(res) - 1):
-        a = list(s)[res[i] : res[i + 1]]
-        p = 0
-        for k in a:
-            if k != ' ':
-                if p % 2 == 0:
-                    answer += a[p].upper()
-                else:
-                    answer += a[p].lower()
-                p += 1
-            else:
-                answer += ' '
-    return answer
+    s = list(s.lower()) # 전부 소문자로 바꾸기
+    # word_start가 단어의 시작
+    # 공백 + 단어시작 > 단어시작 word_start를 지속적으로 갱신하여 
+    # word_start와의 거리 차이를 통해서 인덱스 파악하기
+    word_start = 0
+    for i, x in enumerate(s):
+        if (i - word_start) % 2 == 0:
+            s[i] = s[i].upper() 
+        # index를 i, i + 1 사용할 때는 len(s) - 1까지 사용하나
+        # enumerate때문에 불가하므로 조건문에 추가
+        if i + 1 < len(s) and x == ' ' and s[i + 1] != ' ': 
+            word_start = i + 1
+    return ''.join(s)
