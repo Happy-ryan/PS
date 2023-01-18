793c690829060087ec4376a15dfad8d61bf4d247
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-18 16:54:34 +0900

    [Gold IV] Title: 타일 채우기, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,21 +1,18 @@
-def solution(n):
-    answer = 0
-    dp = [0] * (n + 1)
-    if n == 1:
-        return 0
-    dp[0] = 1
-    dp[2] = 3
-    # n이 홀수인 경우 직사각형의 면적 3*n으로 홀수가 나온다.
-    # 각 나무토막의 넓이는 2이므로 절대로 가로가 홀수인 경우에는 전체를 채울 수 없다.
-    for k in range(4, n+1, 2):
-        for x in range(2, k+1, 2):
-            if x == 2:
-                dp[k] += 3 * dp[k - x]
-            else:
-                dp[k] += 2 * dp[k - x]
-    
-    answer = dp[n]
-    return answer
+import sys
+input = sys.stdin.readline
 
-N = int(input())
-print(solution(N))
+n = int(input())
+dp = [0] * (n + 1)
+dp[0] = 1
+dp[1] = 0
+# n이 홀수일 때는 타일을 채울 수 없다.
+# n이 홀수면 면적 3 * n > 홀수
+# 나무도착 하나의 넓이 2이므로 짝수 + 짝수 +... + 짝수 = (무조건) 짝수
+for k in range(2, n + 1, 2):
+    for x in range(2, k + 1, 2):
+        if x == 2:
+            dp[k] += dp[k - x] * 3
+        else:
+            dp[k] += dp[k - x] * 2
+
+print(dp[n])
