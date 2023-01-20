a9eec8ef50c7773d8f29200e9fabc12aca869a11
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-20 16:29:54 +0900

    [unrated] Title: 귤 고르기, Time: 14.14 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -2,11 +2,9 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     ans =[]
-    for key, value in Counter(tangerine).items():
-        ans.append((value, key)) # (귤의 갯수, 귤의 종류)
-    ans.sort( key = lambda x : x[0], reverse = True)
-    for num, _ in ans:
-        if k > num:
+    dic = Counter(tangerine)
+    for num in sorted(dic.values(), reverse = True):
+        if k > num: # k > num : num를 다 지급해도 k- num 남는다.
             k -= num
             answer += 1
         else: # k < num : 지금 num에서 k개 전부 고르기 가능 : break
