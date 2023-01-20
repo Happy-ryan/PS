a09f8c8131bc865bddf9d1aca2ee3bc3c5b196b5
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-20 14:42:30 +0900

    [level 2] Title: 프린터, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,24 +1,18 @@
 from collections import deque
 
-def check(p, q): # 내 뒤에 나보다 큰 우선순위가 있니?
-    flag = True
-    for i in range(len(q)):
-        if p < q[i][1]:
-            flag = False
-    return flag
-
 def solution(priorities, location):
-    answer = deque([])
-    q = deque([])
-    for idx, p in enumerate(priorities):
-        q.append((idx, p))
-    while q:
-        idx, p = q.popleft()
-        if check(p, q):
-            answer.append((idx, p))
-        else:
-            q.append((idx, p))
-    
-    for i in range(len(answer)):
-        if answer[i][0] == location:
-            return i + 1
+    # location  = 내가 요청한 문서의 인덱스
+    L = len(priorities)
+    order = 0
+    point = 0
+    while True:
+        if max(priorities) == priorities[point%L]:
+            priorities[point%L] = 0
+            order += 1
+            # point % L : if문 통과 > 우선순위 제일 큰 놈의 인덱스 > 출력
+            # location : 내가 요청 문서의 인덱스
+            # == : 내가 요청한 것이 최고 우선순위를 가지므로 출력한다.
+            if point%L == location:
+                break
+        point += 1   
+    return order
