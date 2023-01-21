969fe3ea1e0b9a91e2af49589b40827b2c3276cb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-21 22:16:51 +0900

    [Bronze I] Title: 스캐너, Time: 132 ms, Memory: 117568 KB -BaekjoonHub

--- 백준/Bronze/3035. 스캐너/스캐너.py
+++ 백준/Bronze/3035. 스캐너/스캐너.py
@@ -2,12 +2,9 @@ R, C, ZR, ZC = map(int, input().split())
 mat = [list(input()) for row in range(R)]
 new_mat = [[0 for col in range(C * ZC)] for row in range(R * ZR)]
 
-for r0 in range(R):
-    for c0 in range(C):
-        x = mat[r0][c0]
-        for r1 in range(r0 * ZR, r0 * ZR + ZR):
-            for c1 in range(c0 * ZC, c0 * ZC + ZC):
-                new_mat[r1][c1] = x
+for i in range(R*ZR):
+  for j in range(C*ZC):
+       new_mat[i][j] = mat[i//ZR][j//ZC]
 
 for row in new_mat:
     print(''.join(row))
