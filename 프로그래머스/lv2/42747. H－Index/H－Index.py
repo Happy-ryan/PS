94103937221021a09af43be11e279a2c033bc397
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-01-25 15:20:59 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,17 +1,3 @@
-# def solution(citations):
-#     answer = 0
-#     citations.sort()
-#     for h in range(0, citations[-1] + 1):
-#         cnt = 0
-#         for cite in citations:
-#             if cite > h:
-#                 cnt += 1
-#         print(cnt)
-#         if h == cnt:
-#             answer = max(answer, h)
-        
-#     return answer
-
 def solution(citations):
     answer = 0
     citations.sort()
