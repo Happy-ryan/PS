5d94f7db7e39107ce68e651a16c1073f9888b9e2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-02-26 23:21:44 +0900

    [Silver III] Title: 빈도 정렬, Time: 148 ms, Memory: 115596 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -2,25 +2,11 @@ from collections import Counter
 n, c = map(int, input().split())
 arr = list(map(int, input().split()))
 dic = Counter(arr)
-
-check = set()
-res = []
-for x in arr:
-    if x not in check:
-        check.add(x)
-        res.append(x)
-# print(dic)
-# print(res)
-
-ans = []
-for i, x in enumerate(res):
-    ans.append((dic[x], i, x))
-    
 # 정렬 기준 : 1. 빈도수(내림차순 - 리버스)  - > 2. 인덱스(올림차순 - 기본)
-# 두 번 정렬할 때는 1 - > 2 : 2조건 먼저 기록 후 1조건 기록
-ans.sort( key = lambda x : x[1]) 
-ans.sort( key = lambda x : x[0], reverse= True)
+# Counter 함수는 인덱스 보장
+ans = list(dic.items())
+ans.sort( key = lambda x : x[1], reverse= True)
 
-for fre, idx, val in ans:
+for val, fre in ans:
     for _ in range(fre):
         print(val, end = ' ')
