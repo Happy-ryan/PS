adf9129eb18eab25610707e5e063d1e164266c27
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-03-23 23:32:26 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,24 @@
+dic = {
+"CU" : "see you",
+":-)":	"I’m happy",
+":-(":	"I’m unhappy",
+";-)": "wink",
+":-P":"stick out my tongue",
+"(~.~)":	"sleepy",
+"TA":	"totally awesome",
+"CCC":	"Canadian Computing Competition",
+"CUZ":	"because",
+"TY":	"thank-you",
+"YW":	"you’re welcome",
+"TTYL":	"talk to you later"   
+}
+
+while True:
+    s = input()
+    if s == "TTYL":
+        print(dic[s])
+        break
+    if s not in dic.keys():
+        print(s)
+    else:
+        print(dic[s])
