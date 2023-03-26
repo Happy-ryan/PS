e0df0e6d07eff21c79236ce9bdba6e9fbb4f93b5
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-03-26 23:29:50 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+s = input()
+
+dic = {"NLCS": "North London Collegiate School",
+       "BHA": "Branksome Hall Asia",
+       "KIS": "Korea International School",
+       "SJA": "St. Johnsbury Academy" }
+
+print(dic[s])
