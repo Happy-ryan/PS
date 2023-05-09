224e3d1943cc1b48514f22326233dd7668025d7a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-05-09 18:01:04 +0900

    [Bronze V] Title: Serca, Time: 348 ms, Memory: 117836 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/26766
+t = int(input())
+for _ in range(t):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")  
+    print("    @ @    ")
+    print("     @     ") 
