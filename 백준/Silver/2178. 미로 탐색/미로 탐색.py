e33b5217f1ff301007ca8f598a3be623403670b6
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-05-25 01:30:21 +0900

    [Silver I] Title: 미로 탐색, Time: 172 ms, Memory: 117424 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,45 +1,48 @@
-n,m = map(int,input().split())
-# (1,1)에서부터 시작하기 위해서 # 로 벽치기
-graph = ['#'*(m+2)]+['#'+input()+'#'for row in range(n)]+['#'*(m+2)]
-# print(graph)
-# print(graph[1][1])
-
-# bfs를 위해서 deque 사용
+# https://www.acmicpc.net/problem/2178
 from collections import deque
 
-# bfs에서 dfs의 vistied와 유사한 기능을 하는 것을 in_queue로 정의
-in_queue =  [[ False for col in range(m+1)] for row in range(n+1)]
+n, m = map(int, input().split())
+board = [list(input()) for row in range(n)]
 
-# 미로 이동하면서 이동한 거리 저장 dp
-dist = [[0 for col in range(m+1)] for row in range(n+1)]
 
-# 현재 (1,1) 위치 체크하기
-q = deque([(1,1)])
-in_queue[1][1] = True
-dist[1][1] = 1
-
-# 이동하기
-dr = [-1,1,0,0] # 좌//우
-dc = [0,0,-1,1] # 상//하
-
-# 이동했을 때 board안에 있는지 체크
-def bound_check(r, c):
-    return 1 <= r <= n and 1 <= c <= m
-
-# q가 있을 때까지 체크:
-while q:
-    r,c = q.popleft()
-    for x in range(4):
-        nr = r + dr[x]
-        nc = c + dc[x]
-        # graph 안에 존재 &
-        # 1으로 있는 곳만 이동가능 &
-        # in_queue 가 False ,즉 방문하지 않은 곳
-        if bound_check(nr,nc) and \
-            graph[nr][nc] == '1' and \
-                not in_queue[nr][nc]:
-                q.append((nr,nc))
+def bfs(n, m, board):
+    in_queue = [[False for col in range(m)] for row in range(n)]
+    dist = [[0 for col in range(m)] for row in range(n)]
+    
+    def check(r, c):
+        return 0<= r < n and 0<= c < m and\
+            not in_queue[r][c] and board[r][c] == "1"
+    
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    
+    q = deque([])
+    start = (0, 0) # 시작점 고정
+    q.append(start)
+    cnts = []
+    cnt = 0
+    dist[start[0]][start[1]] = 1
+    while q:
+        # cnt += 1 # start에서 연견될 1 전부 찾음 -> 문제 특성 상 전부 연결되어있으므로 1의 개수가 cnt가 된다.
+        cr, cc = q.popleft()   
+        in_queue[cr][cc] = True
+        # dist[cr][cc] = cnt 
+        for k in range(4):
+            nr = cr + dr[k]
+            nc = cc + dc[k]
+            if check(nr, nc):
                 in_queue[nr][nc] = True
-                dist[nr][nc] = dist[r][c] +1
+                q.append((nr, nc))
+                dist[nr][nc] = dist[cr][cc] + 1
+                
+                
+    # bfs 종료되면 cnts에 너비우선탐색값 저장하기
+    cnts.append(cnt)
+        
+    return dist[n - 1][m - 1]
 
-print(dist[n][m])
+
+def solution(n, m, board):
+    return bfs(n, m, board)
+    
+print(solution(n, m, board))
