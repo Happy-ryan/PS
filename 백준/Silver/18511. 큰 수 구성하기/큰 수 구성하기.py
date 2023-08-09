babad9c25ce432e0bba3775d10b6003ac2d8787d
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-08-09 20:06:40 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 184 ms, Memory: 117792 KB -BaekjoonHub

---
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+visited = []
+best_num = 0
+
+def dfs(level, depth):
+    global best_num
+    if level == depth:
+        return int(''.join(map(str, visited)))
+    for i in range(k):
+        visited.append(nums[i])
+        num = dfs(level + 1, depth)
+        if num is not None and num <= n:
+            best_num = max(num, best_num)
+        visited.pop()
+    return best_num
+    
+print(max([dfs(0, i) for i in range(1, len(str(n)) + 1)]))
