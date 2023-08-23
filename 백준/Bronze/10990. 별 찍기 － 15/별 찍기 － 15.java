db41e9ac8f5359fcd29ba46d1a190abe6ddf35f2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-08-23 23:29:21 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 412 ms, Memory: 20748 KB -BaekjoonHub

--- 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -4,26 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n;
+        int n = sc.nextInt();
 
-        n = sc.nextInt();
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 0;  j < (n - i); j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
-            if(i == 1){
-            System.out.print('*');
-            System.out.println();
-            }
-            else{
-            System.out.print('*');
-            for(int k = 1; k <= (2 *( i - 1) - 1); k++ ){
+            System.out.print("*");
+            for (int k = 1; k <= 2 * (i - 1) - 1; k++) {
                 System.out.print(" ");
             }
-            System.out.print('*');
-            System.out.println();
+            if (i > 1) {
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
