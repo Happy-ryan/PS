f56f75a72122381cb26e1bf9b0082c1fc3733cda
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-08-24 23:51:23 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        a /= 2;
+
+        System.out.print(Math.min(a, b));
+    }
+}
