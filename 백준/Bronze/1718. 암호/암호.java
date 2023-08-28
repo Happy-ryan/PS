7ffc562cf2fee1a2af29945461f159636fd774bb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-08-28 23:18:58 +0900

    [Bronze II] Title: 암호, Time: 552 ms, Memory: 297664 KB -BaekjoonHub

--- 백준/Bronze/1718. 암호/암호.java
+++ 백준/Bronze/1718. 암호/암호.java
@@ -8,27 +8,15 @@ public class Main {
         String s2 = sc.nextLine();
         String ans = "";
 
-        for (int idx = 0; idx < s1.length(); idx++) {
-            char x = s1.charAt(idx);
-            // 공백 유지
+        for (int i = 0; i < s1.length(); i++) {
+            char x = s1.charAt(i);
             if (x == ' ') {
                 ans += ' ';
-            } 
-            // 공백 제외 평문의 아스키코드
-            else {
-                int orgin = (int) s1.charAt(idx);
-                // 암호화 키의 알파벳 순서 -> 암호화 키 안에서 돌기 때문에 모듈러 연산 도입! -> 나머지
-                int p = idx % s2.length();
-                int key = (int) s2.charAt(p) - (int) 'a' + 1;
-                // 암호화 아스키 코드
-                int en = orgin - key;
-                // en이 a보다 작아질 경우 z로 되돌아가 값 출력
-                if (97 <= en && en <= 122) {
-                    ans += (char) en;
-                } else {
-                    en += 26;
-                    ans += (char) en;
-                }
+            } else {
+                int orgin = (int) x;
+                int key = (int) s2.charAt(i % s2.length()) - (int) 'a' + 1;
+                int en = orgin - key + (orgin - key < 'a' ? 26 : 0);
+                ans += (char) en;
             }
         }
         System.out.println(ans);
