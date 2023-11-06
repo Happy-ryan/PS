22a48457d956e7d39d3528bf5badf55d5e8b4bb2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-11-06 15:10:04 +0900

    [Gold V] Title: 신기한 소수, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -9,15 +9,15 @@ def isPrime(n):
     return True
 
 
-def dfs(cur, n):
+def dfs(cur, level, n):
     if not isPrime(cur):
         return
-    if len(str(cur)) == n:
+    if level == n:
         print(cur)
         return
     # 1부터 시작
     for num in range(1, 10):
-        dfs(cur * 10 + num, n)
+        dfs(cur * 10 + num, level + 1, n)
 
 # cur 현재 가장 왼쪽의 위치에 해당함.
 # 0과 1은 가장 왼쪽에 올 수 있는 숫자에 해당하지 않음.
@@ -25,4 +25,5 @@ def dfs(cur, n):
 # cur를 2부터 9까지 전부 살펴봐야함.
 n = int(input())
 for cur in range(2, 10):
-    dfs(cur, n)
+    level = 1
+    dfs(cur, level, n)
