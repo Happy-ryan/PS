a3b900d7360ba323d999dee09db89683bb8ae110
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-11-26 20:13:36 +0900

    [Silver II] Title: N과 M (10), Time: 124 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,4 +1,19 @@
-# https://www.acmicpc.net/problem/15664
+def dfs(level, start, path):
+    if level == m:
+        ans.add(tuple(path))
+        print(*path)
+        return
+
+    for i in range(start, n):
+        if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+            continue
+
+        visted.append(nums[i])
+        used[i] = 1
+        dfs(level + 1, i + 1, path + [nums[i]])
+        visted.pop()
+        used[i] = 0
+
 
 n, m = map(int, input().split())
 nums = sorted(list(map(int, input().split())))
@@ -7,22 +22,4 @@ ans = set()
 visted = []
 used = [0] * n
 
-
-def dfs(level, m):
-    if level == m:
-        row = visted.copy()
-        if row == sorted(row) and tuple(row) not in ans:
-            ans.add(tuple(row))
-            print(*row)
-        return
-
-    for idx, num in enumerate(nums):
-        if not used[idx]:
-            visted.append(num)
-            used[idx] = 1
-            dfs(level + 1, m)
-            visted.pop()
-            used[idx] = 0
-
-
-dfs(0, m)
+dfs(0, 0, [])
