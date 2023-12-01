92e9735f64719fa90334fa85c6535a0335f5e675
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-01 14:33:24 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 3860 ms, Memory: 218508 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,9 +1,14 @@
 from collections import deque
+import sys
+import math
 
-n, m, r = map(int, input().split())
+input = sys.stdin.readline
+
+n, m, R = map(int, input().split())
 # min(n, m) % 2 = 0
 # 최솟값은 반드시 짝수
 board = [list(map(int, input().split())) for _ in range(n)]
+
 
 # 2차원 회전 - 좌표집합 밀기로 접근
 def extract(r1, c1, r2, c2):
@@ -18,6 +23,7 @@ def extract(r1, c1, r2, c2):
         dq.append((row, c1))
     return dq
 
+
 def rotate(dq):
     tmps = deque([])
     for r, c in dq:
@@ -25,7 +31,8 @@ def rotate(dq):
     tmps.rotate(-1)
     for idx, (r, c) in enumerate(dq):
         board[r][c] = tmps[idx]
-        
+
+
 k = min(n, m) // 2
 
 cr, cc = 0, 0
@@ -34,29 +41,30 @@ c1 = cc
 r2 = cr + n - 1
 c2 = cc + m - 1
 
+
 def simulate(r1, c1, r2, c2):
-    
     if r1 == k and c1 == k:
         return
 
     dq = extract(r1, c1, r2, c2)
-    # print(f"r1: {r1}, c1:{c1}, r2: {r2}, c2: {c2}")
-    # print(f"회전 전: {dq}")
     rotate(dq)
-    # print(f"회전 board")
-    # for row in board:
-    #     print(*row)
-    # print("=" * 10)
-    
-    r1 += 1
-    c1 += 1
-    r2 -= 1
-    c2 -= 1
-    
-    simulate(r1, c1, r2, c2)
 
-for _ in range(r):
-    simulate(r1,c1,r2,c2)
+    simulate(r1 + 1, c1 + 1, r2 - 1, c2 - 1)
+
+
+# r에 대한 주기파악 > 최소공배수 > 최소공배수만큼 돌면 맨처음 상태로 돌아옴
+mod = 1
+for r in range(k):
+    num = m * 2 + (n - 2) * 2
+    mod = mod * num // math.gcd(mod, num)
+
+    m -= 2
+    n -= 2
+
+R %= mod
+
+for _ in range(R):
+    simulate(r1, c1, r2, c2)
 
 for row in board:
     print(*row)
