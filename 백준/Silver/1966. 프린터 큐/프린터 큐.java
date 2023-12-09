703de388ed56a066c510a0cda0c296a60a4436a2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-09 16:21:57 +0900

    [Silver III] Title: 프린터 큐, Time: 176 ms, Memory: 15372 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer inputNM = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(inputNM.nextToken());
+            int m = Integer.parseInt(inputNM.nextToken());
+
+            StringTokenizer inputPriority = new StringTokenizer(br.readLine());
+
+            Deque<Integer> priorities = new ArrayDeque<>(m);
+            Deque<Integer> documents = new ArrayDeque<>();
+            for (int k = 0; k < n; k++) {
+                priorities.add(Integer.parseInt(inputPriority.nextToken()));
+                documents.add(k);
+            }
+            System.out.println(printQueue(priorities, documents, m));
+        }
+    }
+
+    private static int printQueue(Deque<Integer> prioritys, Deque<Integer> documents, int m) {
+        List<Integer> answer = new ArrayList<>();
+        while (!documents.isEmpty()) {
+            int maxPriority = maxPriority(prioritys);
+            if (maxPriority == prioritys.getFirst()) {
+                answer.add(documents.pollFirst());
+                prioritys.pollFirst();
+            } else {
+                prioritys.add(prioritys.pollFirst());
+                documents.add(documents.pollFirst());
+            }
+        }
+        return answer.indexOf(m) + 1;
+    }
+
+    private static int maxPriority(Deque<Integer> prioritys) {
+        int res = 0;
+        for (int pri : prioritys) {
+            res = Math.max(res, pri);
+        }
+        return res;
+    }
+}
