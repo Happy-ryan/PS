43acc251e4efb6c4607be000f650c0927781de02
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-21 14:58:16 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 436 ms, Memory: 147860 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -2,49 +2,26 @@
 from collections import defaultdict
 n, p = map(int, input().split())
 # 각 줄은 독립적!
-guitar_strings_melody = dict()
+guitar_strings_melody = defaultdict(list)
+
 for _ in range(n):
     string_number, plat_number = map(int, input().split())
-    if string_number not in guitar_strings_melody.keys():
-        guitar_strings_melody[string_number] = []
-        guitar_strings_melody[string_number].append(plat_number)
-    else:
-        guitar_strings_melody[string_number].append(plat_number)
+    guitar_strings_melody[string_number].append(plat_number)
     
-
-def count_finger_number(plats: list[int]):
-    # stack[-1]이 가장 큰수가 되도록 했음. > 마지막에 위치한 것만 소리남!
+def count_finger_number(plats):
     stack = []
     cnt = 0
+
     for plat in plats:
-        if len(stack) == 0:
+        # 새로운 plat이 stack에 마지막이 될 때까지 pop
+        while stack and stack[-1] > plat:
+            stack.pop()
+            cnt += 1
+        # stack이 비어있거나 새로운 plat이 가장 큰 경우        
+        if not stack or stack[-1] < plat:
             stack.append(plat)
             cnt += 1
-        else:
-            # 새로운 plat이 더 크면 넣기!
-            if stack[-1] < plat:
-                stack.append(plat)
-                cnt += 1
-            # 새로운 plat이 더 작은 경우.
-            else:
-                # 새로운 plat이 제일 큰 숫자가 될 때까지 pop
-                while True:
-                    # 전부 빼면 다시 넣기
-                    if len(stack) == 0:
-                        stack.append(plat)
-                        cnt+=1
-                        break
-                    # 빼다가 새로운 plat이랑 마지막 plat이 같으면
-                    # 기존의 plat이므로 넣지 않기
-                    if stack[-1] == plat:
-                        break
-                    # 새로운 plat이 더 크면 넣기
-                    if stack[-1] < plat:
-                        stack.append(plat)
-                        cnt += 1
-                        break
-                    stack.pop()
-                    cnt += 1
+
     return cnt
 
 ans = 0
