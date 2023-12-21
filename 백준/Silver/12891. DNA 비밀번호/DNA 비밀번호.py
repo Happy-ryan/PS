d0cc66105686d8c01c3aaaea35be6ca733ef8c73
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-22 01:40:22 +0900

    [Silver II] Title: DNA 비밀번호, Time: 280 ms, Memory: 113964 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -14,40 +14,23 @@ def count_chracter(part_dna: str):
     return dic
 
 def check(standard_list: list[int], target_dict: dict):
-    check_list = ["A", "C", "G", "T"]
-    for idx, check in enumerate(check_list):
+    for idx, check in enumerate("ACGT"):
         if standard_list[idx] > target_dict[check]:
             return 0
     return 1
 
-# 시간복잡도 슬라이싱 > n^2 > 시간초과
-# 슬라이딩 윈도우..?
-# 기존의 l과 새로운 r을 집중해야한다.
 l = 0
 r = l + p - 1
 target_dict = count_chracter(dna[l : r + 1])
 
 ans = check(standard_list, target_dict)
 
+
 while r < s - 1:
-    if dna[l] == "A":
-        target_dict["A"] -= 1
-    elif dna[l] == "C":
-        target_dict["C"] -= 1
-    elif dna[l] == "G":
-        target_dict["G"] -= 1
-    else:
-        target_dict["T"] -= 1
+    target_dict[dna[l]] -= 1
     l += 1
     r += 1
-    if dna[r] == "A":
-        target_dict["A"] += 1
-    elif dna[r] == "C":
-        target_dict["C"] += 1
-    elif dna[r] == "G":
-        target_dict["G"] += 1
-    else:
-        target_dict["T"] += 1
+    target_dict[dna[r]] += 1
     
     ans += check(standard_list, target_dict)
     
