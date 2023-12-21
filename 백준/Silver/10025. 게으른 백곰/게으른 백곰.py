a2a96b3e33404fc973b2b4dd01105945b81f363f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-21 14:09:03 +0900

    [Silver III] Title: 게으른 백곰, Time: 204 ms, Memory: 126112 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -6,12 +6,14 @@ number_line = [0] * (inf + 1)
 for _ in range(n):
     g, x = map(int, input().split())
     number_line[x] = g
-    
+# 좌표가 0base임.
 init_mid = k
 l, r = init_mid - k, init_mid + k
 cnt = sum(number_line[: r + 1])
 ans = cnt
 # 시간복잡도 O(N)
+# 마지막 범위 주의하기! (ex) k = 3 / inf - 3, inf -2, inf - 1, inf
+# inf - 3까지 와야함, +1 안하면 inf - 4가 마지막 범위임.
 for mid in range(init_mid + 1, inf - k + 1):
     # 기존의 l 삭제
     cnt -= number_line[l]
