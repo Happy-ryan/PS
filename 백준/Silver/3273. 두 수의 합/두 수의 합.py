e35d095e27f414d110d12f62876c32c5bec22a8f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-14 17:43:39 +0900

    [Silver III] Title: 두 수의 합, Time: 148 ms, Memory: 121564 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,20 +1,25 @@
-import sys
-input = sys.stdin.readline
+# https://www.acmicpc.net/problem/3273
 
 n = int(input())
-a = sorted(list(map(int,input().split())))
-# 주어진 입력을 정렬해도 문제가 없을지 확인하기
+nums = list(map(int, input().split()))
+nums.sort()
 x = int(input())
 
-cnt = 0
-sum = 0
-r = n-1
+def tow_pointer():
+    cnt = 0
+    l, r = 0, n - 1
+    while l < r:
+        k = nums[l] + nums[r]
+        if k == x:
+            cnt += 1
+            k -= nums[l]
+            k -= nums[r]
+            l += 1
+            r -= 1
+        elif k > x:
+            r -= 1
+        elif k < x:
+            l += 1
+    return cnt
 
-for l in range(n):
-  while r >0  and a[l]+a[r] > x: # 정렬한 상태/ r-끝점 /r -= 1 : 두 쌍의 합이 작아진다. 즉 두 쌍의 합이 x보다 크다를 의미함. 따라서 부등호 방향 a[l]+a[r]>x가 되는 것
-    sum += a[r] # a[l] + a[r] < x x보다 작다는 거니까  r+=1이되어야하는데 r은 n-1끝점이다. 그러므로 불가능
-    r -= 1
-  if l < r and a[l] + a[r] == x:
-    cnt += 1
-
-print(cnt)
+print(tow_pointer())
