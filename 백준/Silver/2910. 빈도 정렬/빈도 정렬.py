e2efbd668b2ee3530a0c4fc73f8580f4df9f8612
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-19 16:29:22 +0900

    [Silver III] Title: 빈도 정렬, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,11 +1,20 @@
-from collections import Counter
-n, c = map(int, input().split())
-arr = list(map(int, input().split()))
-dic = Counter(arr)
-# 정렬 기준 : 1. 빈도수(내림차순 - 리버스)  - > 2. 인덱스(올림차순 - 기본)
-# Counter 함수는 인덱스 보장
-ans = list(dic.items())
-ans.sort( key = lambda x : x[1], reverse= True)
+from collections import defaultdict
 
-for val, fre in ans:
-    print(f"{val} " * fre, end = '')
+n, c = map(int, input().split())
+nums = list(map(int, input().split()))
+
+# defaultdict을 사용할 때 기본값으로 리스트를 사용하고 싶다면 아래와 같이 정의
+# [처음 나온 인덱스, 빈도]
+fre = defaultdict(lambda: [0, 0])
+
+for idx, num in enumerate(nums):
+    if fre[num][0] == 0:
+        fre[num][0] = idx + 1
+    fre[num][1] += 1
+
+arr = list(fre.items())
+arr.sort(key=lambda x: (-x[1][1], x[1][0]))
+
+
+for key, value in arr:
+    print(f"{key} " * value[1], end="")
