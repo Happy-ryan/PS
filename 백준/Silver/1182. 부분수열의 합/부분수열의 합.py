837e52ff42d936e8e5765a76cd4dfb64cb59da0b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-19 13:36:40 +0900

    [Silver II] Title: 부분수열의 합, Time: 232 ms, Memory: 110436 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,14 +1,15 @@
-# https://www.acmicpc.net/problem/1182
-# 시간복잡도
-# -> 20C1+20C2+...20C19 2^20 = 1000 1000 = 1000000(백만)
-from itertools import combinations
+#시간복잡도
+# n*(nC1 + nC2 + ... nCn) = n * 2^n = 20 * 1000 * 1000
 
 n, s = map(int, input().split())
-arr = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+
+from itertools import combinations
 
 cnt = 0
 for x in range(1, n + 1):
-    for row in combinations(arr, x):
+    for row in combinations(nums, x):
         if sum(row) == s:
             cnt += 1
 
