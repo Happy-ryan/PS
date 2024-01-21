b160d6e4ed2a0348af17a4bd63e4a7bb8a085c28
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-21 20:12:52 +0900

    [Bronze III] Title: ACM 호텔, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,9 +1,17 @@
-import sys
+# https://www.acmicpc.net/problem/10250
+# 방 선호: 엘리베이터에서 가까운 순 > 같다면 낮은 층
+
+
+def solution(H: int, W: int, N: int):
+    # 1순위: 거리1 채우고 > 거리2 채우고.... 몇 호실?
+    # H만큼 각 거리를 채우게 된다.
+    XX = (N // H) + 1 if N % H != 0 else N // H
+    # 2순위: 몇 층?
+    YY = N % H if N % H != 0 else H
+    return f"{YY}{XX:02}"
+    
+
 T = int(input())
-for _ in range(T) :
-    H, W, N = map(int, sys.stdin.readline().split())
-    cnt = 1 # 호수를 나타낸다
-    while N > H :
-        N = N -H # while문 빠져나오면 층수를 의미한다.
-        cnt += 1
-    print(f"{N*100+cnt}") 
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    print(solution(H, W, N))
