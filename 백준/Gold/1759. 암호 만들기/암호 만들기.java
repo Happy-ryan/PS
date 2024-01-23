6202de0e7f4a9dedff6f2aed5f3c5c0f922a1371
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-23 14:41:10 +0900

    [Gold V] Title: 암호 만들기, Time: 148 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int L, C;
+    private static List<String> alpabets;
+
+    private static int[] used;
+    private static List<String> ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer input = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(input.nextToken());
+        C = Integer.parseInt(input.nextToken());
+
+        alpabets = new ArrayList<>(C);
+        StringTokenizer inputAlpabet = new StringTokenizer(br.readLine());
+        for(int i = 0; i < C; i++){
+            alpabets.add(inputAlpabet.nextToken());
+        }
+        // 증가하는 순이므로 정렬 필요함(자바 정렬 - Collections.sort(배열)
+        Collections.sort(alpabets);
+
+        used = new int[C];
+        ans = new ArrayList<>();
+        dfs(0, 0);
+    } 
+
+    private static void dfs(int level, int idx){
+        if (level == L){
+            // 모음 1개이상, 자음 2개이상
+            if (check(ans)){
+                prettyPrint(ans);
+            }
+            return;
+        }
+
+        for(int i = idx; i < C; i++){
+            if(used[i] == 0){
+                used[i] = 1;
+                ans.add(alpabets.get(i));
+                dfs(level + 1, i + 1);
+                used[i] = 0;
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+
+        private static boolean check(List<String> ans){
+        int 모음 = 0;
+        int 자음 = 0;
+        for(String string : ans){
+            if ("aeiou".contains(string)){
+                모음++;
+            }
+            else{
+                자음++;
+            }
+        }
+
+        if (모음 >=1 && 자음>= 2){
+            return true;
+        }
+        return false;
+    } 
+
+    private static void prettyPrint(List<String> ans){
+        StringBuilder sb = new StringBuilder();
+        for(String string : ans){
+            sb.append(string);
+        }
+        System.out.println(sb.toString());
+    }  
+}
