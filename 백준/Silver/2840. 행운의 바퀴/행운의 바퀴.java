8c2a92c517b1f2af3c87b4dab0104caa06de0789
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-28 23:47:35 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 212 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -5,7 +5,8 @@ public class Main {
     public static int[] numbers;
     public static char[] alpabets;
 
-    public static Deque<Integer> fortuneWheel = new ArrayDeque<>();
+    private static int mod = 0;
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,11 +25,6 @@ public class Main {
             alpabets[i] = al;            
         }
     
-
-        for (int i = 0; i < n; i++) {
-            fortuneWheel.add(i);
-        }
-
        char[] realWheel = new char[n];
 
        for (int i = 0; i < n; i++) {
@@ -38,15 +34,14 @@ public class Main {
         int finalCharIndex = 0;
         boolean 한칸_알파벳_중복 = false;
         for(int i = 0; i < k; i++) {
-            roate(numbers[i]);
+            mod = roate(numbers[i]);
             // System.out.print(fortuneWheel.getFirst() + ":" + alpabets[i]+"\n");
             // realWheel에 ?가 아니면서 이미 들어있던 문자와 다른 경우에는 알 수 없는 wheel이 된다.
-            if (realWheel[fortuneWheel.getFirst()] != '?' && realWheel[fortuneWheel.getFirst()] != alpabets[i]) {
+            if (realWheel[mod] != '?' && realWheel[mod] != alpabets[i]) {
                 한칸_알파벳_중복 = true;
             }
-
-            realWheel[fortuneWheel.getFirst()] = alpabets[i]; // 휠에 알파벳 할당
-            finalCharIndex = fortuneWheel.getFirst();
+            realWheel[mod] = alpabets[i]; // 휠에 알파벳 할당
+            finalCharIndex = mod;
         }
 
         if (isDuplicateAlpabet(realWheel) || 한칸_알파벳_중복) {
@@ -55,45 +50,39 @@ public class Main {
         else {
             prettyPrint(realWheel, finalCharIndex);
         }
-
     }
-    // 화살표는 인덱스0번만 가르킨다!
-    // ABC - 원판 
-    // 각 1번씩 회전의 상태 변화 관찰: ABC > CAB > BCA > ABC
-    // 맨뒤가 맨앞으로 오도록!
+    // mod는 default 0
     // 바퀴의 칸 수만큼 회전시키면 원상태로 회복
-    public static void roate(int count) {
-        count %= n;
-        for (int t = 0; t < count; t++){
-            int tmp = fortuneWheel.removeLast();
-            fortuneWheel.addFirst(tmp);
-        }
+    public static int roate(int count) {
+        mod += count;
+        mod %= n;
+        return mod;
     }
 
     public static void prettyPrint(char[] realWheel, int finalCharIndex) {
         int mod = finalCharIndex;
         StringBuilder br = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            mod %= n;
+            if (mod == -1) {
+                mod = n - 1;
+            }
             br.append(realWheel[mod]);
-            mod++;
+            mod--;
         }
-        System.out.println(br.toString());
+        System.out.println(br);
     }
 
     public static boolean isDuplicateAlpabet(char[] realWheel) {
-        Set<Character> charSet = new HashSet<>();
+        int[] used = new int[26];
         for (char ch: realWheel) {
             if (ch == '?') {
                 continue;
             }
-            if (charSet.contains(ch)) {
+            used[(int)ch - 'A'] += 1;
+            if (used[(int)ch - 'A'] > 1) {
                 return true;
-            } else {
-                charSet.add(ch);
             }
         }
         return false;
     }
-
 }
