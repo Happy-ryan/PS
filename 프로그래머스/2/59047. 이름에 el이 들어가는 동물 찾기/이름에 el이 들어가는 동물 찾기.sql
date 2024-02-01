4b3d2451b4cacd35a7754c33cf7d6121927af60f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-01 14:47:41 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -2,5 +2,5 @@
 select ANIMAL_ID, NAME
 from ANIMAL_INS
 where (NAME like "%el%" or NAME like "%El%" or NAME like "%EL%" or NAME like "%eL%") and
-    ANIMAL_TYPE like "Dog"
+    ANIMAL_TYPE like 'Dog'
 order by NAME asc;
