4cd195ea52b1ee4ccd3b63b1b236cc47fb99e344
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-07 16:50:24 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+# 동물보호소에 '들어온'
+# 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty
+# 아이디, 이름, 성별, 중성화 여부
+# 아이디순 조회
+select ANIMAL_ID, NAME, SEX_UPON_INTAKE
+from ANIMAL_INS
+where NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+order by ANIMAL_ID;
