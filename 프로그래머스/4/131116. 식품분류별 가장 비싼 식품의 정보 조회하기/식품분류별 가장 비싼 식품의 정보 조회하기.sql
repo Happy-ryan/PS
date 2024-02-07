04bae62a996c857c2c37c66c5b53626d47c07dfb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-07 20:38:55 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -3,7 +3,6 @@
 # 식품별 비싼거 - group by + 집계함수
 # 식품분류 -  '과자', '국', '김치', '식용유' - where
 # 식품 가격 기준 내림차순
-
 with temp as (
     select CATEGORY, max(PRICE) as PRICE
     from FOOD_PRODUCT
@@ -16,8 +15,8 @@ inner join temp T on (P.CATEGORY = T.CATEGORY and P.PRICE = T.PRICE)
 where P.CATEGORY in ('과자', '국', '김치', '식용유')
 order by P.PRICE desc;
 
-# 각 카테고리별 최대값 가져오기
-# subtring_index(group_concat(P.PRODUCT_NAME order by P.PRICE desc), ',', 1)
+# 각 카테고리별 최대값 가져오기 = substring_index + group_concat
+# substring_index(group_concat(P.PRODUCT_NAME order by P.PRICE desc), ',', 1)
 select P.CATEGORY, MAX(P.PRICE) as MAX_PRICE, 
        substring_index(group_concat(P.PRODUCT_NAME ORDER BY P.PRICE DESC), ',', 1)
        AS PRODUCT_NAME
@@ -25,3 +24,16 @@ from FOOD_PRODUCT P
 where P.CATEGORY in ('과자', '국', '김치', '식용유')
 group by P.CATEGORY
 order by P.PRICE desc;
+
+SELECT P.CATEGORY, 
+       P.PRICE AS MAX_PRICE, 
+       P.PRODUCT_NAME
+FROM FOOD_PRODUCT P
+WHERE P.CATEGORY IN ('과자', '국', '김치', '식용유')
+AND (P.CATEGORY, P.PRICE) IN (
+    SELECT P2.CATEGORY, MAX(P2.PRICE)
+    FROM FOOD_PRODUCT P2
+    WHERE P2.CATEGORY IN ('과자', '국', '김치', '식용유')
+    GROUP BY P2.CATEGORY
+)
+ORDER BY P.PRICE DESC;
