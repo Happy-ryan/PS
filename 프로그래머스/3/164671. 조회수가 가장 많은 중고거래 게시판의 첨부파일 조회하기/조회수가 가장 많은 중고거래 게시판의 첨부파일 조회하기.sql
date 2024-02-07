749e018e334437f03bc6af51c5db77e8a919b51a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-07 17:39:38 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -3,16 +3,15 @@
 # 조회수가 가장 높은 중고거래 게시물
 # 조회수가 가장 높다 > 서브쿼리절로 추출 or 윈도우함수
 # FILE ID를 기준으로 내림차순 
-select CONCAT('/home/grep/src/', BOARD_ID, '/',FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+select CONCAT("/home/grep/src/", BOARD_ID, "/",FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
 from USED_GOODS_BOARD 
 inner join USED_GOODS_FILE USING(BOARD_ID) 
 WHERE VIEWS = (SELECT MAX(VIEWS) 
                FROM USED_GOODS_BOARD )
-ORDER BY FILE_ID DESC
+ORDER BY FILE_ID DESC;
 
-# select concat('/home/grep/src/', B.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) as FILE_PATH
-# from USED_GOODS_BOARD B
-# inner join USED_GOODS_FILE F on B.BOARD_ID = F.BOARD_ID
-# where B.VIEWS = (select MAX(VIEWS) 
-#                  from USED_GOODS_BOARD)
-# order by F.FILE_ID desc;
+select concat('/home/grep/src/', B.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) as FILE_PATH
+from USED_GOODS_BOARD B
+inner join USED_GOODS_FILE F on B.BOARD_ID = F.BOARD_ID
+where B.VIEWS = (select max(VIEWS) from USED_GOODS_BOARD )
+order by F.FILE_ID desc;
