0b6e924836d054c23a9cd7b13f5a4e4528e2cecd
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-08 01:54:42 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 548 ms, Memory: 57200 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+    private static int n;
+    private static char[][] board;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        n = sc.nextInt();
+        board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        
+        f(0, 0, n);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(board[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void f(int r, int c, int w) {
+
+        if (w == 3) {
+            for (int i = r; i < r + 3; i++) {
+                for (int j = c; j < c + 3; j++) {
+                    if (i == r + 1 && j == c + 1) {
+                        board[i][j] = ' ';
+                    } else {
+                        board[i][j] = '*';
+                    }
+                }
+            }
+            return;
+        }
+
+        int d = w / 3;
+
+        f(r, c, d);
+        f(r, c + d, d);
+        f(r, c + 2 * d, d);
+
+        f(r + d, c, d);
+        f(r + d, c + 2 * d, d);
+
+        f(r + 2 * d, c, d);
+        f(r + 2 * d, c + d, d);
+        f(r + 2 * d, c + 2 * d, d);
+    }
+}
