9598f7519bc04760218ba64676615c59c6fda74a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-12 14:44:44 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+# 우유와 요거트 동시 구입한 장바구니
+# 장바구니의 id 조회
+# id 순으로 오름차순
+with milk as (
+    select CART_ID
+    from CART_PRODUCTS
+    where NAME in ('Milk')
+), yogurt as (
+    select CART_ID
+    from CART_PRODUCTS
+    where NAME in ('Yogurt')
+)
+
+select distinct(M.CART_ID)
+from milk M
+inner join yogurt Y on M.CART_ID = Y.CART_ID
+order by M.CART_ID asc;
