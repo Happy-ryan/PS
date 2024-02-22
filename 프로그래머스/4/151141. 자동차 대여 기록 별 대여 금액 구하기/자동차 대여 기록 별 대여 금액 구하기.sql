92e96d860a4c44dddc1cd42be710a06620510e3e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-22 15:11:18 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -23,3 +23,68 @@ from temp T
 left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P 
         on T.CAR_TYPE = P.CAR_TYPE and T.DURATION_TYPE = P.DURATION_TYPE
 order by FEE desc, T.HISTORY_ID desc;
+
+
+
+# 자동차 종류 - 트럭 / 자동차 대여기록에 대해서 대여기록별로 대여금액을 구하기
+# 대여기록id, 대여금액 리스트 / 대여금액 기준 내림차순, 기록id기준 내림차순
+
+with temp as (
+    select H.CAR_ID, H.HISTORY_ID, C.CAR_TYPE,
+    case
+        when timestampdiff(day, H.START_DATE, H.END_DATE) + 1 < 7 then null
+        when timestampdiff(day, H.START_DATE, H.END_DATE) + 1 < 30 then '7일 이상'
+        when timestampdiff(day, H.START_DATE, H.END_DATE) + 1 < 90 then '30일 이상'
+        else '90일 이상' 
+    end as DURATION_TYPE,
+    (timestampdiff(day, H.START_DATE, H.END_DATE) + 1) * C.DAILY_FEE as TOTAL_FEE
+    from CAR_RENTAL_COMPANY_CAR C
+    inner join CAR_RENTAL_COMPANY_RENTAL_HISTORY H on C.CAR_ID = H.CAR_ID
+    where C.CAR_TYPE = '트럭'
+)
+
+select T.HISTORY_ID,
+    round((T.TOTAL_FEE * (100 - ifnull(P.DISCOUNT_RATE, 0)) / 100), 0) as FEE
+from temp T
+-- temp에 7일 이하는 null로 들어감..그래서 inner join을 하면 사라짐
+left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P 
+    on T.CAR_TYPE = P.CAR_TYPE and T.DURATION_TYPE = P.DURATION_TYPE
+    order by FEE desc, T.HISTORY_ID desc;
+
+
+
+# HISTORY_ID	FEE
+# 724	6336960
+# 681	5356240
+# 630	4791360
+# 558	4404960
+# 653	3793160
+# 722	2909040
+# 594	1888600
+# 680	1524750
+# 714	1118150
+# 591	1118150
+# 556	813200
+# 610	672000
+# 676	568000
+# 527	535000
+# 720	532000
+# 710	532000
+# 701	504000
+# 640	428000
+# 641	426000
+# 628	426000
+# 623	336000
+# 602	336000
+# 673	321000
+# 631	321000
+# 654	284000
+# 618	214000
+# 586	214000
+# 581	214000
+# 546	214000
+# 716	140000
+# 711	107000
+# 705	107000
+# 627	107000
+# 524	107000
