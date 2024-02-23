63873f554ec964d01b80476781d7e1c59537bfe3
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-23 14:58:53 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -60,4 +60,65 @@ select 23 as HOUR
 
 select temp.HOUR, ifnull(temp2.COUNT, 0) as COUNT
 from temp
-left join temp2 on temp.HOUR = temp2.HOUR;
+left outer join temp2 on temp.HOUR = temp2.HOUR;
+
+
+# 0 ~ 23시 각 시간대별로 입양이 몇건
+# 시간대 순으로
+with temp as (
+    select 0 as HOUR
+    union
+    select 1 as HOUR
+    union
+    select 2 as HOUR
+    union
+    select 3 as HOUR
+    union
+    select 4 as HOUR
+    union
+    select 5 as HOUR
+    union
+    select 6 as HOUR
+    union
+    select 7 as HOUR
+    union
+    select 8 as HOUR
+    union
+    select 9 as HOUR
+    union
+    select 10 as HOUR
+    union
+    select 11 as HOUR
+    union
+    select 12 as HOUR
+    union
+    select 13 as HOUR
+    union
+    select 14 as HOUR
+    union
+    select 15 as HOUR
+    union
+    select 16 as HOUR
+    union
+    select 17 as HOUR
+    union
+    select 18 as HOUR
+    union
+    select 19 as HOUR
+    union
+    select 20 as HOUR
+    union
+    select 21 as HOUR
+    union
+    select 22 as HOUR
+    union
+    select 23 as HOUR
+), temp2 as (
+    select hour(DATETIME) as HOUR, count(*) as COUNT
+    from ANIMAL_OUTS
+    group by HOUR
+)
+
+select T.HOUR, ifnull(P.COUNT, 0) as COUNT
+from temp T
+left join temp2 P on T.HOUR = P.HOUR;
