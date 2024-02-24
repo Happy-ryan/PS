7425b3ccc27d672759ee6e01aa12dfc4b6a24aaf
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-24 11:32:13 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -9,6 +9,7 @@ with temp as (
     group by WRITER_ID
     having count >= 3
 )
+
 select U.USER_ID,
        U.NICKNAME,
        concat(U.CITY,
@@ -24,3 +25,29 @@ select U.USER_ID,
 from temp T
 inner join USED_GOODS_USER U on T.WRITER_ID = U.USER_ID
 order by U.USER_ID desc;
+
+
+
+
+# 중고거래게시물 3건 이상 등록한 사용자의 사용자id, 닉네임, 전체주소, 전화번호 조회
+# 전체주소: 시 / 도로명 / 상세주소
+# 전화번호 xxx-xxxx-xxxxx
+# 회원id기준 내림차순
+
+with temp as(
+    select WRITER_ID, count(*) as COUNT
+    from USED_GOODS_BOARD
+    group by WRITER_ID
+    having count(*) >= 3
+)
+
+select  T.WRITER_ID, U.NICKNAME,
+        concat(U.CITY, ' ', U.STREET_ADDRESS1, ' ', U.STREET_ADDRESS2) as 전체주소,
+        concat(substring(TLNO, 1, 3),
+               '-',
+                substring(TLNO, 4, 4),
+               '-',
+              substring(TLNO, 8)) as 전화번호
+from temp T
+inner join USED_GOODS_USER U on T.WRITER_ID = U.USER_ID
+order by T.WRITER_ID desc;
