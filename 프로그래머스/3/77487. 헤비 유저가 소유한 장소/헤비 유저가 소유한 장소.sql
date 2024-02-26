a190286b9091244d6bd8e1f206736106c9598d7a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-26 16:31:00 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -2,13 +2,13 @@
 # 공간을 둘 이상 등록한 사람을 헤비유저
 # 헤비유저가 등록한 공간의 정보를 아이디순으로 
 with temp as (
-    select HOST_ID, count(*) as COUNT
+    select HOST_ID
     from PLACES
     group by HOST_ID
-    having COUNT >= 2
+    having count(*) >= 2
 )
 
-select P.ID, P.NAME, P.HOST_ID
-from temp T
-inner join PLACES P on T.HOST_ID = P.HOST_ID
-order by P.ID asc;
+select ID, NAME, HOST_ID
+from PLACES 
+where HOST_ID in (select HOST_ID from temp)
+order by ID asc;
