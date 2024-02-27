d6f4b6052fec300282d8b2895ba3057dc360f406
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-27 15:21:34 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,18 +1,27 @@
 n = int(input())
-mod = 1000000000
-dp = [[ 0 for col in range(11) ] for row in range(n+1)]
-# print(dp)
-#dp[i][j]  i : 자릿수 j : 숫자
-for j in range(1,10):
-    dp[1][j] = 1 # 1,2,3,4,...9 각 1개 = 왜냐하면 j가 숫자를 의미하므로 1씩 각각 써줘야한다. / dp[1] = 9 ,, dp[1][j] =1
-# print(dp)
-for i in range(2,n+1):
-    for j in range(0,10):
-        if j == 0:
-            dp[i][j] = (dp[i-1][1])%mod #j가 0일 때는 앞자리에 1만 올 수 있다.
-        elif j == 9:
-            dp[i][j] = (dp[i-1][8])%mod #j가 9일 때는 앞자리에 8만 올 수 있다.
-        else:
-            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%mod
 
-print(sum(dp[n])%mod)
+def solution(n):
+    # dp[i][j] i번자리수일 때 마지막 수가 j일 때 만들어지는 계단수의 개수
+    inf = int(1e9)
+    dp = [[-inf for _ in range(10)] for _ in range(n + 1)]
+    mod = 1000000000
+    dp[0][0] = 0
+    for i in range(1, n + 1):
+        if i == 1:
+            for j in range(10):
+                if j == 0:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = 1
+        else:
+            for j in range(10):
+                if j == 0:
+                    dp[i][j] = (dp[i - 1][j + 1]) % mod
+                elif j == 9:
+                    dp[i][j] = (dp[i - 1][j - 1]) % mod
+                else:
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod
+                    
+    return (sum(dp[n])) % mod
+        
+print(solution(n))
