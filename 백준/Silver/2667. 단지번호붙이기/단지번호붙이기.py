537d152cc455306797d2c0d09a181ea3583a5f5e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-27 17:27:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,49 @@
-n = int(input())
-apt = ['#'*(n+2)]+['#'+input()+'#' for row in range(n)]+\
-        ['#'*(n+2)]
-# print(apt)
-# print(apt[1])
-# print(apt[1][1])
-in_queue = [[False for col in range(n+1)] for row in range(n+1)]
-cnt = []
-
-# 이동방향
-dr =[-1,1,0,0]
-dc =[0,0,-1,1]
-# apt 2차원 배열 안에서만 움직이도록 체크
-def apt_check(r,c):
-    return 1<= r <= n and 1<= c <= n
-
 from collections import deque
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if in_queue[i][j] == True:
-            pass
-        elif in_queue[i][j] == False and apt[i][j] == '0':
-            pass
-        else:
-            q = deque([(i,j)]) 
-            in_queue[i][j] = True # 방문 체크
-            result = 1
-            while q:
-                r,c = q.popleft()
-                for k in range(4):
-                    nr = r + dr[k]
-                    nc = c + dc[k]
-                    if apt_check(nr,nc) and\
-                        not in_queue[nr][nc] and\
-                            apt[nr][nc] =='1':
-                            q.append((nr,nc))
-                            in_queue[nr][nc] = True
-                            result +=1
-            cnt.append(result)
-print(len(cnt))
-for x in sorted(cnt):
-    print(x)
+n = int(input())
+board = [list(input()) for _ in range(n)]
+
+def solution(n, board):
+    
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    
+    in_queue = [[False for _ in range(n)] for _ in range(n)]
+    dq = deque([])
+    
+    def in_range(r, c):
+        return  0 <= r < n and 0 <= c < n
+    
+    def bfs(r, c):
+        cnt = 0
+        
+        dq.append((r, c))
+        in_queue[r][c] = True
+        cnt += 1
+        
+        while dq:
+            cr, cc = dq.popleft()
+            for k in range(4):
+                nr = cr + dr[k]
+                nc = cc + dc[k]
+                if in_range(nr, nc) and not in_queue[nr][nc] and board[nr][nc] == '1':
+                    dq.append((nr, nc))
+                    in_queue[nr][nc] = True
+                    cnt += 1
+                    
+        return cnt
+                    
+    cnts = []
+    for r in range(n):
+        for c in range(n):
+            if in_queue[r][c] or board[r][c] == '0':
+                continue
+            cnt = bfs(r, c)
+            cnts.append(cnt)
+            
+    cnts.sort()
+    print(len(cnts))
+    for cnt in cnts:
+        print(cnt)
+
+solution(n, board)
