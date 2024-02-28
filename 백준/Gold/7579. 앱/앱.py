db003014f91a7181dc68768771a8fdc6e651ab2f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-28 23:54:22 +0900

    [Gold III] Title: 앱, Time: 140 ms, Memory: 114680 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -2,24 +2,31 @@ n, m = map(int, input().split())
 memories = list(map(int, input().split()))
 costs = list(map(int, input().split()))
 
-# dp[i]: 비용이 i일 때 지울 수 있는 메모리의 최대 크기
-# 왜 기준으로 cost를 선택했냐? 
-# 메모리기준 dp[i]: 메모리가 i일 때 최소 비용
-# 메모리는 10,000,000 길이 배열이 필요함
-# M 10,000,000 최대 확보해야하는 메모리의 최대 크기
-# N * M
-# N 100개 각갹의 메모리 10,000,000
-# M 10,000,000
-max_cost = 10004;
-dp = [-1] * max_cost
-dp[0] = 0
+def solution(n, m, memories, costs):
+    # m = 10,000,000 / max_cost = 100 * 100
+    # dp[i][j] i번째 앱(item)을 선택하고 j비용(weight) 일 때, 최대 메모리(value)의 값
+    # 특정 비용일 때, 메모리를 최대한 비활성화 시키고 싶다!
+    memories = [0] + memories
+    costs = [0] + costs
+    inf = int(1e9)
+    max_cost = 100 * 100
+    dp = [[-inf for _ in range(max_cost + 1)] for _ in range(n + 1)]
+    
+    dp[0][0] = 0
+    for i in range(1, n + 1):
+        w, v = costs[i], memories[i]
+        for j in range(max_cost, -1, -1):
+            dp[i][j] = dp[i - 1][j]
+            if j - w >= 0:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + v)
+    
+    min_cost = inf
+    # cost의 범위는 0 ~ 100 * 100d이다!!!!!!!!
+    for cost in range(max_cost + 1):
+        if dp[n][cost] >= m:
+            min_cost = min(min_cost, cost)
+            
+    return min_cost
 
-for memory, cost in zip(memories, costs):
-    for i in reversed(range(cost, max_cost)):
-        if dp[i-cost] != -1:
-            dp[i] = max(dp[i], dp[i-cost]+memory)
 
-for i in range(max_cost):
-    if dp[i] >= m:
-        print(i)
-        break
+print(solution(n, m, memories, costs))
