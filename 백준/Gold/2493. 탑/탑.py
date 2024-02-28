f5aa5a78e68e5e6ead5e50b953c00a53b542ebf8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-28 16:08:39 +0900

    [Gold V] Title: 탑, Time: 348 ms, Memory: 198704 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,24 +1,31 @@
-# # https://www.acmicpc.net/problem/2493
-# # 시간복잡도: 이중for문 > O(n^2)
-
 n = int(input())
 tops = list(map(int, input().split()))
 
-stack = []
-res = [0] * n
-
-for idx, nxt_top in enumerate(tops):
-    while stack:
-        # 스택의 마지막 탑과 nxt_top을 비교했을 때 nxt_top이 더 크면
-        # 스택의 마지막 탑보다 작은 쪽, 즉 좌측 탑은 전혀 볼 필요가 없다! 
-        # 그러므로 nxt_top이 더 크면 좌측의 탑을 전부 빼도 상관없다.
-        if stack[-1][0] < nxt_top:
-            stack.pop()
-        else:
-            res[idx] = stack[-1][1] + 1
-            break
-        
-    stack.append((nxt_top, idx))
+def solution(n, tops):
+    # 6(0) 9(0) 5(2) 7(2) 4(4)
+    # [6] 9 <- 5 .. 6은 의미가 없다. 왜? 9가 6을 가린다!! 6을 필요가 없어짐.
+    # 들어올 탑의 왼쪽에 나보다 작은 탑이 존재해서는 안된다!
+    stack = []
+    memo = [0 for _ in range(n)]
+    for idx, top in enumerate(tops):
     
-print(*res)
+        # 새로운 탑의 높이보다 왼쪽에 작은 탑들은 제외!
+        while stack and stack[-1][0] <= top:
+            stack.pop()
+        
+        # 스택이 비어있다면 왼쪽 탑이 없다. 수신하는 탑이 존재하지 않는다.
+        if len(stack) == 0:
+            memo[idx] = 0
+        # 스택이 있다. 왼쪽에 내 신호를 수신하는 탑이 존재하는데
+        #   (~~~5의 입장에서는 필요없음) 9 5
+        # 9가 5의 레이저 수신
+        # stack에서 가장 위에 있는 것이 레이저를 수신한다.
+        else:
+            memo[idx] = stack[-1][1] + 1
+            
+        stack.append((top, idx))
+        
+        
+    return memo
 
+print(*solution(n, tops))
