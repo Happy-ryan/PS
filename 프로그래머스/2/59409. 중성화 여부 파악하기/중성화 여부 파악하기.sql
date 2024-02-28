1d7061422862bcab042d607e677a8b355770f310
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-28 13:39:52 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -2,3 +2,10 @@
 select ANIMAL_ID, NAME,
     if(SEX_UPON_INTAKE like 'Neutered%' or SEX_UPON_INTAKE like 'Spayed%', 'O', 'X') as '중성화'
 from ANIMAL_INS;
+
+
+select ANIMAL_ID, NAME,
+        if(SEX_UPON_INTAKE like 'Neutered%' or SEX_UPON_INTAKE like 'Spayed%',
+            'O', 'X') as 중성화
+            
+from ANIMAL_INS;
