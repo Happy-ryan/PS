f4713c9f652223a6b2754123e0e37a60b2df76bf
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-28 22:37:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 316 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,11 +1,21 @@
-n,k = map(int,input().split())
-arr = [list(map(int,input().split())) for row in range(n)]
-# print(arr)
-dp = [[-1 for col in range(k+1)] for row in range(n+1)]
-dp[0][0] = 0
-for i in range(1,n+1):
-    dp[i] = dp[i-1].copy()
-    for j in range(k+1):
-        if dp[i-1][j] >= 0 and j + arr[i-1][0] <= k:
-            dp[i][j+arr[i-1][0]] = max(dp[i][j+arr[i-1][0]],dp[i-1][j]+arr[i-1][1])
-print(max(dp[n]))
+n, k = map(int, input().split())
+infos = [list(map(int, input().split())) for _ in range(n)]
+
+def solution(n, k, infos):
+    # dp[i][j] i번째 물건 선택하고 무게j 일때, 최대 가치(value)
+    infos = [0] + infos
+    inf = int(1e9)
+    dp = [[-inf for _ in range(k + 1)] for _ in range(n + 1)]
+    
+    # 
+    dp[0][0] = 0
+    for i in range(1, n + 1):
+        w, v = infos[i]
+        for j in range(k, -1, -1):
+            dp[i][j] = dp[i - 1][j]
+            if j - w >= 0:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + v)
+    
+    return max(dp[n])
+
+print(solution(n, k, infos))
