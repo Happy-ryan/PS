1c50addc9f2a35f1b035de14629707e4317e9324
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-28 14:49:00 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -6,12 +6,26 @@ with temp as (
     select CAR_ID, max(START_DATE) as START_DATE
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY
     group by CAR_ID
-    having timestampdiff(day, '2022-10-01', START_DATE) >= 0
-    order by CAR_ID desc
+    having timestampdiff(day, '2022-10-01', START_DATE) + 1 >= 1
 )
 
 select T.CAR_ID
 from temp T
 inner join CAR_RENTAL_COMPANY_CAR C on T.CAR_ID = C.CAR_ID
 where C.CAR_TYPE in ('세단')
+order by T.CAR_ID desc;
+
+# 세단 중 10월에 대여를 시작한 기록
+# id 중복이 없어야하며, id기준으로 내림차순
+# 대여기록, 판매기록 등은 반드시 id의 중복이 있으니 주의해라!
+with temp as (
+    select CAR_ID, max(START_DATE) as START_DATE
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    group by CAR_ID
+    having year(START_DATE) = 2022 and month(START_DATE) = 10
+)
+select T.CAR_ID
+from temp T
+inner join CAR_RENTAL_COMPANY_CAR C on T.CAR_ID = C.CAR_ID
+where C.CAR_TYPE = '세단'
 order by T.CAR_ID desc;
