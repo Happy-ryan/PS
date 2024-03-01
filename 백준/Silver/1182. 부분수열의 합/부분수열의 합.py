445843d32106de1d33848db9fd6fe05c1e110d6f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-03-01 23:34:50 +0900

    [Silver II] Title: 부분수열의 합, Time: 196 ms, Memory: 113168 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,30 @@
-#시간복잡도
-# n*(nC1 + nC2 + ... nCn) = n * 2^n = 20 * 1000 * 1000
-
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
 
+def solution(n, s, nums):
+    
+    sum_val = 0
+    cnt = 0
+    def dfs(idx):
+        nonlocal cnt, sum_val
+        if idx == n:
+            if sum_val == s:
+                cnt += 1
+            return
+        
+        # 선택했다.
+        sum_val += nums[idx]
+        dfs(idx + 1)
+        sum_val -= nums[idx]
+        
+        # 선택 안했다.
+        dfs(idx + 1)
+    
+    dfs(0)
+    
+    # 공집합!!
+    if s == 0:
+        cnt -= 1
+    return cnt
 
-from itertools import combinations
-
-cnt = 0
-for x in range(1, n + 1):
-    for row in combinations(nums, x):
-        if sum(row) == s:
-            cnt += 1
-
-print(cnt)
+print(solution(n, s, nums))
