d4c92ae1ea46a5f141a72ecdb04248129731dc72
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-03-02 11:42:59 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -9,17 +9,47 @@ with temp as (
     select  year(S.SALES_DATE) as YEAR,
             month(S.SALES_DATE) as MONTH,
             # 년/월로 그룹바이를 하다보니 id에 중복이 발생한! group by id하지 않앗음..
+            # 판매기록 등에서는 id가 중복되어있을 수 있다.
+            # 정답 안나오면 distinct부터 항상 의심하기!
             count(distinct I.USER_ID) as PURCHASED_USERS
     from USER_INFO I
     inner join ONLINE_SALE S on I.USER_ID = S.USER_ID
     where year(I.JOINED) = 2021
     group by YEAR, MONTH
 ), total_users as (
-    select count(*) as TOTAL
+    select count(USER_ID) as TOTAL
     from USER_INFO
     where year(JOINED) = 2021
 )
 
-select T.YEAR, T.MONTH, T.PURCHASED_USERS, round(T.PURCHASED_USERS / U.TOTAL, 1) as PURCHASED_RATIO
+select  T.YEAR, T.MONTH, T.PURCHASED_USERS,
+        round(T.PURCHASED_USERS / U.TOTAL, 1) as PURCHASED_RATIO
 from temp T, total_users U
-order by t.YEAR asc, t.MONTH asc;
+order by T.YEAR asc, T.MONTH asc;
+
+
+# 2021년 가입한 전체 회원 중 상품을 구매한 회원의비율
+# 년, 월별로 출력
+# 두번째반올림(round(1)) /년도기준 
+
+with total as (
+    select count(*) as COUNT
+    from USER_INFO
+    where year(JOINED) = 2021
+), member as (
+    # 판매기록, 대여기록--기록 id 중복 반드시 존재!
+    select year(S.SALES_DATE) as YEAR,
+            month(S.SALES_DATE) as MONTH,
+            # group by 년 / 월 ..예시 2022-01 user_id 1이 아이가 2번 구매! > 1명처리으로 처리해여함
+            # 구매한 사람의 수를 원하므로 2명이 아니라 1명으로 처리해여함
+            count(distinct I.USER_ID) as COUNT
+    from USER_INFO I
+    inner join ONLINE_SALE S on I.USER_ID = S.USER_ID	
+    where year(I.JOINED) = 2021
+    group by YEAR, MONTH
+    
+)
+
+select M.YEAR, M.MONTH, M.COUNT as PUCHASED_USERS, 
+        round(M.COUNT / T.COUNT,1) as PUCHASED_RATIO
+from member M, total T;
