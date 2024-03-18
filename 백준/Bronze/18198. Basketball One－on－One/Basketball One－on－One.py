e1bfa7d1d1b3fb442cdb4addfbb837457a8f8abb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-03-18 23:59:25 +0900

    [Bronze IV] Title: Basketball One-on-One, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.py
+++ 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.py
@@ -1,14 +1,10 @@
 score = input()
-A_val = 0
-B_val = 0
+A_val = B_val = 0 
 
-for i in range(len(score)):
+for i in range(0, len(score), 2):
     if score[i] == "A":
         A_val += int(score[i + 1])
-    elif score[i] == "B":
+    else:
         B_val += int(score[i + 1])
 
-if A_val > B_val:
-    print("A")
-else:
-    print("B")
+print("A" if A_val > B_val else "B")
