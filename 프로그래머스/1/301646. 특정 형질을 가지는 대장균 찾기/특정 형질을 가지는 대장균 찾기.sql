f46d99decb1cbcc5588e05f54c8fc512cd196b93
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-04-30 14:10:52 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,5 +1,11 @@
 -- 코드를 작성해주세요
-
-select count(ID) as COUNT
+-- substring(1010, -1, 1) = 0
+-- substring(1010, -2, 1) = 1
+-- substring(1010, -3, 1) = 0
+-- substring(1010, -4, 1) = 1
+    
+select count(*) as COUNT
 from ECOLI_DATA
-where (GENOTYPE & 1 = 1 or GENOTYPE & 4 = 4) and (GENOTYPE & 2 = 0)
+where   (substring(conv(GENOTYPE, 10, 2), -1, 1) = 1 or
+        substring(conv(GENOTYPE, 10, 2), -3, 1) = 1) and
+        (substring(conv(GENOTYPE, 10, 2), -2, 1) = 0)
