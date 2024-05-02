04e4a811adc7022ddb8005718bc5d8e2afa3055e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-02 17:35:45 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 작성해주세요
+-- 2022년 / 평가점수 가장 높은 사원 정보 
+-- 상 + 하반기 점수의 합
+
+with total_year as (
+    select  EMP_NO, 
+            sum(SCORE) as TOTAL_SCORE,
+            row_number() over (order by sum(SCORE) desc) as RN
+    from HR_GRADE
+    group by EMP_NO
+)
+
+select T.TOTAL_SCORE as SCORE, E.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
+from total_year as T, HR_EMPLOYEES as E
+where T.RN = 1 and T.EMP_NO = E.EMP_NO;
