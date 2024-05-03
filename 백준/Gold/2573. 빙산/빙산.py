fd4b39faedd425a3878bd07bb5a0c8146d9ab991
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-03 18:27:18 +0900

    [Gold IV] Title: 빙산, Time: 1036 ms, Memory: 212184 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,84 +1,119 @@
-# 물의 영역을 모조리 찾고 물의 영역에서 4방향에 존재하는 빙하의 높이를 줄인다!
-# 1단계: 물의 영역을 찾는 그래프 탐색
-# 2단계: 물의 영역이 빙하에 미치는 영향 갱신하기
-from collections import deque
-import sys
-sys.setrecursionlimit(10**5)
-
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
-# 배열의 첫 번째행과 열, 마지막 행과 열은 0으로만 채워짐
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
-def in_range(r, c, n, m):
-    return 0 <= r < n and 0 <= c < m
-
-
-def find_ice(n, m):
-    
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    
-    def dfs(r, c):
-        visited[r][c] = True
-        cnt[0] += 1 # 방문시 +1
-        ices.append((r, c)) # 방문시 좌표처리
-        
-        for k in range(4):
-            nr = r + dr[k]
-            nc = c + dc[k]
-            if in_range(nr, nc, n, m) and not visited[nr][nc] and board[nr][nc] != 0:
-                dfs(nr, nc)
-
-    ices = []
-    cnts = []
-    for r in range(n):
-        # 열은 m이다! 행과열 다를 때 주의하기!
-        for c in range(m):
-            if visited[r][c] or board[r][c] == 0:
-                continue
-            cnt = [0]
-            dfs(r, c)
-            cnts.append(cnt[0])
-
-    return ices, len(cnts)
-
-
+from collections import deque
 
 def solution(n, m, board):
-
-    t = 0
-    while True:
+    # 0. 빙하의 덩어리 개수 판단
+    # 0-1. 빙하의 덩어리 1개 > 1 진행
+    # 0-2. 빙하의 덩어리 2개 이상 > 종료
+    # 1. 물들이 존재하는 좌표들 찾기
+    # 2. 빙하 녹이기
+    # 빙하 덩어리와 물들의 좌표를 찾는 방법 > bfs / dfs
+    
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    
+    
+    def in_range(r, c):
+        return 0 <= r < n and 0 <= c < m
+    
+    
+    def find_water():
+        # 찾을 때마다 갱신
+        visited = [[False for _ in range(m)] for _ in range(n)]
         
-        ices, ice_group = find_ice(n, m)
+        water_grid = []
+        
+        def bfs(r, c):
+            dq = deque([])
+            
+            dq.append((r, c))
+            visited[r][c] = True
+            water_grid.append((r, c))
+            
+            while dq:
+                cr, cc = dq.popleft()
+                for k in range(4):
+                    nr = cr + dr[k]
+                    nc = cc + dc[k]
+                    if in_range(nr,nc) and board[nr][nc] == 0 and not visited[nr][nc]:
+                        dq.append((nr, nc))
+                        visited[nr][nc] = True
+                        water_grid.append((nr, nc))
                 
-        if ice_group >= 2:
-            return t
+        for r in range(n):
+            for c in range(m):
+                if visited[r][c] or board[r][c] != 0:
+                    continue
+                bfs(r, c)
         
-        if ice_group == 0:
-            return 0
+        return water_grid
+            
+    # 녹일 때 주의점은 한 방에 녹여야한다!
+    # 찔끔찔끔 녹이면 높이가 0이 되는 지점에서 문제가 발생하게 된다!
+    def melting(ices_gird):
+        melting_board = [[0 for _ in range(m)] for _ in range(n)]
         
-        t += 1
-        melt = []
-        for r, c in ices:
-            cnt = 0
+        for r, c in ices_gird:
             for k in range(4):
                 nr = r + dr[k]
                 nc = c + dc[k]
-                if in_range(nr, nc, n, m) and board[nr][nc] == 0:
-                    # r, c가 현재 빙하 위치이며 nr, nc는 물을 찾는 곳임
-                    # 빙하의 높이가 줄어들어야함으로 nr, nc를 줄이면 안된다.
-                    cnt += 1
-            melt.append((r, c, cnt))
-        # 빙하를 동시에 녹여야한다.
-        # 하나씩 녹이면 높이가 1인 경우, 먼저0으로 변경되어 다른 빙하에 영향을 주기때문이다.
-        for i, j, cnt in melt:
-            board[i][j] = max(0, board[i][j] - cnt)
-                
-        # for row in board:
-        #     print(row)
-        # print("=")
+                if in_range(nr, nc) and board[nr][nc] == 0:
+                    melting_board[r][c] -= 1
+                    
+        for r in range(n):
+            for c in range(m):
+                # 0이 한계
+                board[r][c] = max(board[r][c] + melting_board[r][c], 0)
+        
+    
+    def find_glacier():
+        # 찾을 때마다 갱신
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        ices = []
+        
+        def bfs(r, c):
+            dq = deque([])
+            
+            dq.append((r, c))
+            visited[r][c] = True
+            cnt = 1
+            ices.append((r, c))
+            
+            while dq:
+                cr, cc = dq.popleft()
+                for k in range(4):
+                    nr = cr + dr[k]
+                    nc = cc + dc[k]
+                    if in_range(nr,nc) and board[nr][nc] != 0 and not visited[nr][nc]:
+                        dq.append((nr, nc))
+                        visited[nr][nc] = True
+                        cnt += 1
+                        ices.append((nr, nc))
+            return cnt
+        
+        
+        cnts = []        
+        for r in range(n):
+            for c in range(m):
+                if visited[r][c] or board[r][c] == 0:
+                    continue
+                cnt = bfs(r, c)
+                cnts.append(cnt)
+        
+        return len(cnts), ices
+    
+
+    
+    t = 0
+    while True:
+        cnt, ices = find_glacier()
+        if cnt >= 2:
+            return t
+        if cnt == 0:
+            return 0
+        melting(ices)
+        t += 1
 
 print(solution(n, m, board))
