a0d716fac573fe299187a2e31d2d39adb10ae6f8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-04 09:59:58 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 111952 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,14 +1,37 @@
-# 주어진 동전으로 특정한 값 만들기 - 순서x한 경우, DP
 n, k = map(int, input().split())
-moneys = [ int(input()) for _ in range(n)]
-# print(moneys)
+coins = [int(input()) for _ in range(n)]
 
-dp = [0] * (k + 1)
-dp[0] = 1
+# 순서가 영향을 안주는 동전 유형! + 경우의 수 
+# 순서가 영향을 안준다 -> 코인을 한번 쓰고 / 다른 코인을 쓰면 / 썻던 코인을 쓰면 안돼
+# 111111 / 55 / 111  (x)
+# 중복 사용 가능!
+def solution(n, k, coins):
+    # 중복사용 가능 + 경우의 수 + 순서가 상관 없는 경우
+    # dp[money] += dp[money - coin]
+    # 경우의 수 - 0으로 한다!
+    dp = [0 for _ in range(k + 1)]
+    
+    dp[0] = 1
+    # 코인을 중복사용은 가능하되, 쓰고나면 다음에는 쓰지 못함
+    # 그래서 코인이 먼저 소비시켜야함!
+    for coin in coins:
+        for money in range(1, k + 1):
+            if money - coin >= 0:
+                dp[money] += dp[money - coin]
+                
+    return dp[k]
 
-for money in moneys: # 순서가 없어야하므로 주어진 동전들을 깐다.
-    for target in range(1, k + 1):
-        if target >= money:
-            dp[target] += dp[target - money]
+def solution_1(n, k, coins):
+    # 중복사용 가능 + 경우의 수 + 순서가 다르면 다른 경우
+    dp = [0 for _ in range(k + 1)]
+    
+    dp[0] = 1
+    for money in range(1, k + 1):
+        for coin in coins:
+            if money - coin >= 0:
+                dp[money] += dp[money - coin]
+                
+    return dp[k]
+    
 
-print(dp[k])
+print(solution(n, k, coins))
