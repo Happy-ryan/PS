9d71ea8d22554ec010ea1974ff56b0781800d924
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-04 09:19:18 +0900

    [Gold V] Title: 수 고르기, Time: 200 ms, Memory: 112804 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,16 +1,28 @@
 n, m = map(int, input().split())
-a = [int(input()) for _ in range(n)]
-a.sort()
+A = [int(input()) for _ in range(n)]
+
+def solution(n, m, A):
+    # 두 수를 골랐을 때 (같은 수 포함) / 그 차이가 m 이상이면서 제일 작은 경우
+    
+    # 정렬해도 상관없음!
+    # 투포인터 문제는 대부분 이분탐색으로 풀린다.
+    A.sort()
+    # 투포인터 l과 r이 있을 때, l과 r 한 방향으로만 이동할 때!!
+    # 이분탐색 - 쌍을 구할 때는 하나를 고정하고!!
+    def two_pointer(n, m):
+        
+        r = 1
+        ans = int(1e18)
+        for l in range(0, n):
+            while r + 1 < n and (A[r] - A[l] < m or l == r) :
+                r += 1
+                
+            if A[r] - A[l] >= m:
+                ans = min(ans, A[r] - A[l])
+        
+        return ans
+    
+    return two_pointer(n, m)
 
 
-def solution(n, m, a):
-    r = 0
-    ans = int(1e18)
-    for l in range(0, n): # l을 고정
-        while r + 1 < n and (l == r or a[r] - a[l] < m): 
-            r += 1
-        if l != r and a[r] - a[l] >= m:
-            ans = min(ans, a[r] - a[l])
-    return ans
-            
-print(solution(n, m, a))
+print(solution(n, m, A))
