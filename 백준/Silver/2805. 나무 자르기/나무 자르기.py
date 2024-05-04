ac37d61f4e5a510460e032ed2d20af9c3e0612ce
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-04 09:42:45 +0900

    [Silver II] Title: 나무 자르기, Time: 608 ms, Memory: 257900 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,19 +1,36 @@
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 arr = list(map(int,input().split()))
 
-def f(h): # 절단기의 높이가 h일 때 가져갈 수 있는 나무의 총 길이
-    cnt = 0
-    for i in arr:
-        if i >= h:
-            cnt += (i-h)
-    return cnt
+def solution(n, m, arr):
+    # 정렬해도 무관
+    arr.sort()
+    inf = int(1e18)
+    
+    def cutting(h):
+        cnt = 0
+        for i in arr:
+            if i >= h:
+                cnt += (i - h)
+        return cnt
+    
+    # 높이의 최대값
+    # h 15 7개
+    # 왼쪽 True 
+    # h가 x 변수 / cutting 개수 y
+    def binary_search(target):
+        l, r = 0, inf
+        ans = inf
+        while l <= r:
+            m = (l + r) // 2
+            # 7의 왼쪽이 정답! > cutting시 target보다 큰값 중 가장 작은값의 최대값 구하기
+            if cutting(m) >= target:
+                l = m + 1
+                ans = m
+            else:
+                r = m - 1
+                
+        return ans
+    
+    return binary_search(m)
 
-l,r = -1, int(1e12) # l과 r은 절단기의 높이 : r 리턴을 하는데 범위를 보면 r은 절단기의 높이이므로 0~max(arr)이 된다.
-                  # 따라서 l의 범위는 -1~max(arr)-1
-while r-l != 1:
-    m = (r+l)//2
-    if f(m) >= M: # 이렇게 하는 이유 f(h)의 값에 원하는 M이 없을 수 있다. 그러면 조금 더 가져가는게 맞다. 따라서
-                  # l을 리턴해 lower_bound를 출력하면 딱 떨어지게 가져가거나 조금 더 가져가게 된다.
-        l = m
-    else : r = m
-print(l)
+print(solution(n, m, arr))
