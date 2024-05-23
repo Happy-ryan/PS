70685e619df324c3c79de271515e9435a765e1d2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-05-23 23:52:02 +0900

    [Bronze III] Title: Federal Voting Age, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/6845. Federal Voting Age/Federal Voting Age.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t) :
+    y, m, d = map(int, input().split())
+    if y < 1989 : 
+        print("Yes")
+    elif y == 1989 and m < 2 :
+        print("Yes")
+    elif y == 1989 and m == 2 and d < 28 : 
+        print("Yes")
+    else :
+        print("No")
