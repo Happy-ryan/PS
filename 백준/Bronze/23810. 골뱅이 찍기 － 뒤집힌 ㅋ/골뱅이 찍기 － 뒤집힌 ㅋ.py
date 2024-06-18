9b56d702def8fdd665371994272b8120344a2428
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-06-18 22:05:59 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 128 ms, Memory: 111752 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+for _ in range(n):
+    for _ in range(n):
+        print('@@@@@', end='')
+    print()
+    
+for _ in range(n):
+    for _ in range(n):
+        print('@', end='')
+    print()
+    
+for _ in range(n):
+    for _ in range(n):
+        print('@@@@@', end='')
+    print()
+    
+for _ in range(2 * n):
+    for _ in range(n):
+        print('@', end='')
+    print()
