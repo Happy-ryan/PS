05fab36a756a1103e0508127b0d6ddb55e9fc28d
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-06-19 22:18:59 +0900

    [Bronze III] Title: 찬반투표, Time: 96 ms, Memory: 108080 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.py
@@ -0,0 +1,18 @@
+n = int(input())
+votes = list(map(int, input().split()))
+
+def solution(n, votes):
+    a, r, i = 0, 0, 0
+    
+    a += votes.count(1)
+    r += votes.count(-1)
+    i += votes.count(0)
+    
+    if n > 1 and i >= n // 2:
+        return 'INVALID'
+
+    if r >= a:
+        return 'REJECTED'
+    return 'APPROVED'
+
+print(solution(n, votes))
