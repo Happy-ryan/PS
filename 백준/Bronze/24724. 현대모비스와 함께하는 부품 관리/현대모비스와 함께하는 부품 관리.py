504156c3b9ba5fdb2cc95efa6e8dfa8654c99c50
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-09-12 23:26:35 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 192 ms, Memory: 120564 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,6 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    infos = [list(map(int, input().split())) for _ in range(n + 1)]
+    print(f"Material Management {i + 1}")
+    print("Classification ---- End!")
