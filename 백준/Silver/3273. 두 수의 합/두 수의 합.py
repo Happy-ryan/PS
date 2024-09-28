0310f9e854004017919c33141c147c51a67c815c
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-09-28 17:55:15 +0900

    [Silver III] Title: 두 수의 합, Time: 108 ms, Memory: 121564 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,26 +1,36 @@
-# https://www.acmicpc.net/problem/3273
-
 n = int(input())
 nums = list(map(int, input().split()))
-nums.sort()
 x = int(input())
 
-def tow_pointer():
-    cnt = 0
-    l, r = 0, n - 1
-    k = nums[l] + nums[r]
-    while l < r:
-        if k <= x:
-            if k == x:
+def solution(n, nums, x):
+    # 서로 다른 양의 정수 nums
+    # 자연수 x = a_i + a_j를 만족하는 쌍의 수
+    
+    # 순서가 합에 영향을 미치지 않기때문에 정렬하고 시작!
+    nums.sort()
+    
+    # 이중 for문 사용하면 시간초과(n^2)
+    # 이분탐색 / 누적합 / 투포인터 등을 이용해서 시간초과를 해결하는게 문제의 포인트!
+    # 1 2 3 5 7 9 10 11 12
+    
+    # 양방향 투포인터
+    def two_pointer(n, nums, x):
+        cnt = 0
+        
+        l, r = 0, n - 1
+        while l < r and r < n:
+            sum_val = nums[l] + nums[r]
+            if x < sum_val:
+                r -= 1
+            elif x > sum_val:
+                l += 1
+            else:
                 cnt += 1
-            k -= nums[l]
-            l += 1
-            k += nums[l]
-        elif k > x:
-            k -= nums[r]
-            r -= 1
-            k += nums[r]
+                l += 1
+                r -= 1
+                
+        return cnt
+    
+    return two_pointer(n, nums, x)
 
-    return cnt
-
-print(tow_pointer())
+print(solution(n, nums, x))
