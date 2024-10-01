5666ac40f60949319ada562f58b85cc883ad4b6b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-10-01 14:54:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 212 ms, Memory: 115036 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,48 +1,66 @@
 n = int(input())
-num = list(map(int, input().split()))
-plus, minus, mul, div = map(int, input().split())
-visited = [num[0]]
-max_ans = -int(1e18)
-min_ans = int(1e18)
-def dfs(lev, num):
-    global plus, minus, mul, div, max_ans, min_ans
+nums = list(map(int, input().split()))
+cmds = list(map(int, input().split()))
+
+def solution(n, nums, cmds):
+    global res, max_ans, min_ans
     
-    if lev == n:
-        if plus == 0 and minus == 0 and mul == 0 and div == 0:
-            x = visited[-1]
-            max_ans = max(max_ans, x)
-            min_ans = min(min_ans, x)
-            return
-        else: 
-            return
-    visited.append(visited[-1] + num[lev])
-    plus -= 1
-    dfs(lev + 1, num)
-    plus += 1
-    visited.pop()
+    # 더하기 / 빼기 / 곱하기 / 나누기
+    # dfs 돌면서 갱신...더하기 -> 다시 원복..이런 식이 되지 않을까
+    inf = int(1e18)
+    
+    max_ans = -inf
+    min_ans = inf
+    
+    res = nums[0]
+    def dfs(idx):
+        global res, max_ans, min_ans
+        # 주어진 연산을 모두 사용했을 때만 갱신
+        if sum(cmds) == 0:
+            max_ans = max(max_ans, res)
+            min_ans = min(min_ans, res)
+        for k in range(4):
+            # 더하기
+            if k == 0 and cmds[k] > 0:
+                cmds[k] -= 1
+                res += nums[idx]
+                dfs(idx + 1)
+                res -= nums[idx]
+                cmds[k] += 1
+            # 빼기
+            if k == 1 and cmds[k] > 0:
+                cmds[k] -= 1
+                res -= nums[idx]
+                dfs(idx + 1)
+                res += nums[idx]
+                cmds[k] += 1
+            # 곱하기
+            if k == 2 and cmds[k] > 0:
+                cmds[k] -= 1
+                res *= nums[idx]
+                dfs(idx + 1)
+                res //= nums[idx]
+                cmds[k] += 1
+            # 나누기
+            if k == 3 and cmds[k] > 0:
+                # 원상복귀용 > 
+                tmp = res
+                cmds[k] -= 1
+                if res < 0:
+                    res *= -1
+                    res //= nums[idx]
+                    res *= -1
+                else:
+                    res //= nums[idx]
+                dfs(idx + 1)
+                res = tmp
+                cmds[k] += 1
+                
+        return max_ans, min_ans
 
-    visited.append(visited[-1] - num[lev])
-    minus -= 1
-    dfs(lev + 1, num)
-    visited.pop()
-    minus += 1
+    dfs(1)
+    
+    print(max_ans)
+    print(min_ans)
 
-    visited.append(visited[-1] * num[lev])
-    mul -= 1
-    dfs(lev + 1, num)
-    visited.pop()
-    mul += 1
-
-    if visited[-1] < 0:
-        visited.append(-(-visited[-1] // num[lev]))
-    else:
-        visited.append(visited[-1] // num[lev])
-    div -= 1
-    dfs(lev + 1, num)
-    visited.pop()
-    div += 1
-
-dfs(1, num)
-
-print(max_ans)
-print(min_ans)
+solution(n, nums, cmds)
