28d79b6188f6f116b5229d832639ceffc461c5d2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-10-01 12:54:12 +0900

    [Silver III] Title: 순열 사이클, Time: 352 ms, Memory: 118852 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,27 +1,37 @@
-def solution(n, nums):
-    adj = [[] for _ in range(n + 1)]
-    for idx, num in enumerate(nums):
-        adj[idx + 1].append(num)
-
-    visited = [False for _ in range(n + 1)]
-
-    def dfs(start):
-        visited[start] = True
-
-        for nxt in adj[start]:
-            if not visited[nxt]:
-                dfs(nxt)
-    ans = 0
-    for s in range(1, n + 1):
-        if not visited[s]:
-            dfs(s)
-            ans += 1
-    
-    return ans
-
-
+# 순열사이클
 t = int(input())
 
+def solution(n, nums):
+    global cnt
+    # 방향성 존재 - 그래프 생성
+    adj = [[] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        adj[i].append(nums[i - 1])
+    
+    def dfs(num):
+        global cnt
+        # 방문쳌
+        visited[num] = True
+        cnt += 1
+        
+        # 더 깊게..
+        for x in adj[num]:
+            if not visited[x]:
+                dfs(x)
+
+    visited = [False for _ in range(n + 1)]
+    cnts = []
+    for num in range(1, n + 1):
+        cnt = 0
+        # 방문안했으면 dfs 들어가자!
+        if not visited[num]:
+            dfs(num)
+        cnts.append(cnt)
+
+    ans = sum(1 for x in cnts if x != 0)
+    
+    return ans
+    
 for _ in range(t):
     n = int(input())
     nums = list(map(int, input().split()))
