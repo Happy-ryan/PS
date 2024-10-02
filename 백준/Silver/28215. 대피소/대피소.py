8bf280477a2b87a013530e4052c6bb54c320c35d
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-10-02 22:46:40 +0900

    [Silver IV] Title: 대피소, Time: 308 ms, Memory: 111484 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -10,27 +10,19 @@ def solution(n, k, houses):
     def cal_dis(p1, p2):
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
     # 시간복잡도 nCk 
-    ans = []
+    # ans = []
+    ans = inf
     for row in combinations(range(n), k): # N개의 집 ~ K개의 보호소 선택
-        # print(f"선택한 보호소: {row}")
-        dist_1 = []
+        # -inf > 내가 보호소일 경우 거리가 0
+        dist_1 = 0
         for house in range(n): 
-            if house not in row: # 보호소로 선택되지 않은 집에서 보호소까지의 거리 체크
+            if house not in set(row): # 보호소로 선택되지 않은 집에서 보호소까지의 거리 체크
                 dist = inf
                 for x in row:
                     dist = min(dist, cal_dis(houses[house], houses[x]))
-                    # dist_1.append(cal_dis(houses[house], houses[x]))
-                    # print(f"집: {house}, 보호소까지의 거리: {cal_dis(houses[house], houses[x])}")
-                # print(f"집: {house}, 집까지 보호소까자의 최댄거리:{dist} ")
-                dist_1.append(dist)
-                # print(dist_1)
-            # dist_1 = min(dist_1, dist_2)
-            # print(f"==== 집: {house} 끝 ====")
-            # print(f"==== 선택한 보호소의 경우의 수 ==== ")
-        ans.append(max(dist_1))
-        # print(ans)
-    # print(ans)
+                dist_1 = max(dist_1, dist)
+        ans = min(ans, dist_1)
     
-    return min(ans)
+    return ans
     
 print(solution(n, k, houses))
