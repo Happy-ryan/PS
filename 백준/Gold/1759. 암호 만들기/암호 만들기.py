21bf08c483afcc6b0406067848a6fac75ad31406
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-10-02 23:35:12 +0900

    [Gold V] Title: 암호 만들기, Time: 108 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,36 +1,40 @@
-L, C = map(int, input().split())
+l, c = map(int, input().split())
 alpabets = list(input().split())
-# 알파벳이 증가하는 순서로..
-alpabets.sort()
-# 최소 1개의 모음 / 최소 2개의 자음
-# 증가하는 순으로
-def check(arr: list[str]):
-    모음, 자음 = 0, 0
-    for x in arr:
-        if x in 'aeiou':
-            모음 += 1
-        else:
-            자음 += 1
-    if 모음 >= 1 and 자음 >=2:
-        return True
-    return False
-# 
-ans = []
-used = [0] * C
-def dfs(level, idx):
-    if level == L:
-        if check(ans):
-            print(''.join(ans))
-        return
-    
-    for i in range(idx, C):
-        if used[i] == 0:
-            used[i] = 1
-            ans.append(alpabets[i])
-            # 저번에도 i 대신에 idx 넣어서 틀렸는데..또 이 실수?
-            dfs(level + 1, i + 1)
-            ans.pop()
-            used[i] = 0
-        
 
-dfs(0, 0)
+def solution(l, c, alpabets):
+    # 최소 1개의 모음 / 최소 2개의 자음
+    # 정렬된 암호
+    alpabets.sort()
+    
+    ans = []
+    def cal(ans):
+        sum_1, sum_2 = 0, 0
+        for x in ans:
+            if x in 'aeiou':
+                sum_1 += 1
+            else:
+                sum_2 += 1
+        if sum_1 >= 1 and sum_2 >= 2:
+            return True
+        return False
+    
+    # print(alpabets)
+    def back(l, c, idx):
+        if len(ans) == l:
+            if cal(ans):
+                print(''.join(ans))
+            return
+        # idx를 선택했을 때
+        if idx < c:
+            ans.append(alpabets[idx])
+            # print("*: ", ans)
+            back(l, c, idx + 1)
+            ans.pop()
+            # idx를 선택하지 않았을 때
+            back(l, c, idx + 1)
+            
+            # print("**: ",ans)
+        
+    back(l, c, 0)
+    
+solution(l, c, alpabets)
