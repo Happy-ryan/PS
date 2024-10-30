8be62870b0e687cc16f5c483898faba8e592b53f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-10-30 22:18:29 +0900

    [Bronze II] Title: The Cow-Signal, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/14175. The Cow－Signal/The Cow－Signal.py
@@ -0,0 +1,15 @@
+r, c, k = map(int, input().split())
+board = [list(input()) for _ in range(r)]
+
+def solution(r, c, k, board):
+    
+        row = ''
+        for i in range(r):
+            for j in range(c):
+                row += board[i][j] * k
+            for _ in range(k):
+                print(row)
+            row = ''
+            
+
+solution(r, c, k, board)
