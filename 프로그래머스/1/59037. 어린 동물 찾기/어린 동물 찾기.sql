db9aa733c4d29c83c94278e5e3024ee819ca8a7e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-12-21 05:33:21 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION != "Aged"
+ORDER BY ANIMAL_ID;
+
+# 젊은 동물
+select ANIMAL_ID, NAME
+from ANIMAL_INS
+where INTAKE_CONDITION != 'Aged'
+order by ANIMAL_ID asc;
