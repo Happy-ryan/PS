521982410b997b3fe4da5ffa489c4ffdf2abd7f7
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-12-21 05:48:36 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,8 +1,17 @@
 -- 코드를 작성해주세요
 
+# and 와 or 조건이 존재한다는 것!!
+
+# select ID, EMAIL, FIRST_NAME, LAST_NAME
+# from DEVELOPER_INFOS
+# where SKILL_1 = 'Python' or 
+#     SKILL_2 = 'Python' or
+#     SKILL_3 = 'Python'
+# order by ID asc;
+
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPER_INFOS
-where SKILL_1 = 'Python' or 
-    SKILL_2 = 'Python' or
-    SKILL_3 = 'Python'
+where SKILL_1 IN ('Python') or 
+      SKILL_2 IN ('Python') or 
+      SKILL_3 IN ('Python') 
 order by ID asc;
