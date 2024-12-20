080f1c691fda7cadcc1be676f910946177120651
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-12-21 06:19:39 +0900

    [level 1] Title: 가장 비싼 상품 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -16,3 +16,15 @@ with temp as (
 select PRICE as MAX_PRICE
 from temp
 where temp.rn = 1;
+
+# 판매 중인 상품 / 가장 높은 판매가
+with tmp as (
+select PRICE,
+       row_number() over(order by PRICE desc) as rn
+from PRODUCT
+)
+
+select PRICE as MAX_PRICE
+from tmp
+where rn = 1;
+
