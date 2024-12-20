84d1acadbbc51f39d280673705ec2e0b2d5fc896
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-12-21 05:54:15 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,6 +1,15 @@
 -- 코드를 작성해주세요
-select ID, LENGTH
-from FISH_INFO
-order by LENGTH desc, ID asc
-limit 10;
+# select ID, LENGTH
+# from FISH_INFO
+# order by LENGTH desc, ID asc
+# limit 10;
 
+with tmp as (
+    select ID, LENGTH,
+            row_number() over(order by LENGTH desc) as rn
+    from FISH_INFO
+)
+
+select ID, LENGTH
+from tmp
+where rn between 1 and 10;
