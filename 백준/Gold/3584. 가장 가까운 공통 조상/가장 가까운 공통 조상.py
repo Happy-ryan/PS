37dae713f17a6d7300bc422e57902056b5ddb5ac
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-12-29 17:36:49 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 152 ms, Memory: 246080 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -3,36 +3,42 @@ sys.setrecursionlimit(10**5)
 
 t = int(input())
 
-def solution(n, nodes, node1, node2):
+def solution(n, edges, na, nb):
     # 트리 구성
     adj = [[] for _ in range(n + 1)]
-    for a, b in nodes:
+    for a, b in edges:
         adj[a].append(b)
     # 트리 문제 - par 배열
     par = [0] * (n + 1)
-    for p, c in nodes:
+    for p, c in edges:
         par[c] = p
         
     
     # par 배열을 활용하여 부모 찾기
     node1_par = []
     node2_par = []
+    # step = []
+    
     def dfs(cur, node_par):
         visited[cur] = True
+        # step.append(cur)
         
         if not visited[par[cur]]:
             dfs(par[cur], node_par)
-            node_par.append(par[cur])
+            
+        node_par.append(cur) # 나도 공통조상될 수 있음!
 
 
     # print(par)
     visited = [False] * (n + 1)
-    dfs(node1, node1_par)
-    node1_par.append(node1)
+    visited[0] = True
+    dfs(na, node1_par)
+
     # print(node1_par)
     visited = [False] * (n + 1)
-    dfs(node2, node2_par)
-    node2_par.append(node2)
+    visited[0] = True
+    dfs(nb, node2_par)
+
     # print(node2_par)
     
     l = min(len(node1_par), len(node2_par))
@@ -46,7 +52,6 @@ def solution(n, nodes, node1, node2):
 
 for _ in range(t):
     n = int(input())
-    nodes = [list(map(int, input().split())) for _ in range(n - 1)]
-    node1, node2 = map(int, input().split())
-    print(solution(n, nodes, node1, node2))
-    
+    edges = [list(map(int, input().split())) for _ in range(n - 1)]
+    na, nb = map(int, input().split())
+    print(solution(n, edges, na, nb))
