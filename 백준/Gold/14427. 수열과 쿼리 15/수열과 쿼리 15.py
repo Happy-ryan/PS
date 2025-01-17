25bf5792298cde51a17b72ce8b8ef899d182ea03
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-01-17 16:19:11 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 352 ms, Memory: 140428 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
@@ -7,13 +7,8 @@ from heapq import heappush, heappop
 
 def solution(N, A, M, QS):
     A = [0] + A
-    # 1번 명령어 - list의 경우 시간복잡도 문제 없음. 문제는 힙을 쓸 때 해당 인덱스를 가진 값을 어떻게 변경하냐는 이슈 발생
-    # 내 생각에는 pop 시키면서 다른 곳에 저장해두다가 해당 인덱스 나오면 그 값 변경한 후 다른 힙에 넣으면 좋을 것 같음
     
-    # 2번 명령어 - 가장 크기가 작은 값의 인덱스를 가져와야함 / 이 경우 풀 스캔하면 시간초과날 수밖에 없음
-    # (숫자, 인덱스)를 묶어서 가지고 다녀서 heap를 사용해서 pop시키는 것이 합리적이라고 생각
     heap1 = []
-    heap2 = []
     
     for idx, num in enumerate(A):
         if idx == 0:
@@ -22,11 +17,17 @@ def solution(N, A, M, QS):
     
     for q in QS:
         if q[0] == 1:
+            # 배열 관리
             A[q[1]] = q[2]
+            # 기존 값을 굳이 pop할 필요는 없음
+            # 2명령어를 수행할 때 최신화할 예정
             heappush(heap1, (q[2], q[1]))
         else:
             while heap1:
                 num, idx = heappop(heap1)
+                # A[idx]와 num이 일치하지 않는다는 것
+                # 최소로 뽑힌 num이 실제 최솟값이 아니라 예전 최솟값임을 의미한다.
+                # 따라서 그 값은 pop한 상태에서 다시 push할 필요 없다.
                 if A[idx] == num:
                     print(idx)
                     heappush(heap1, (num, idx))
