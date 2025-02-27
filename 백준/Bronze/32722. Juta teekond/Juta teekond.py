9630a9ee2d17010cf477006c22e0fdd5413372ae
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-02-28 04:23:31 +0900

    [Bronze IV] Title: Juta teekond, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.py
@@ -0,0 +1,21 @@
+all = [
+    '162',
+    '165',
+    '182',
+    '185',
+    '362',
+    '365',
+    '385',
+    '382',
+    '385'
+]
+
+a = input()
+b = input()
+c = input()
+
+total = a + b + c
+if total in all:
+    print('JAH')
+else:
+    print('EI')
