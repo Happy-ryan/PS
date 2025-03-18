a4e803509af13a40ee0cbdb4b4c675d0b53bf2b5
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-03-18 23:33:58 +0900

    [Bronze I] Title: 딱지놀이, Time: 176 ms, Memory: 115672 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,45 @@
+n = int(input())
+A = []
+B = []
+for i in range(2 * n):
+    if i % 2 == 0:
+        A.append(list(map(int, input().split())))
+    else:
+        B.append(list(map(int, input().split())))
+
+from collections import Counter
+
+def solution(arr, brr):
+    arr = Counter(arr)
+    brr = Counter(brr)
+
+    for i in range(1, 5):
+        if i not in arr:
+            arr[i] = 0
+        if i not in brr:
+            brr[i] = 0
+            
+    if arr[4] > brr[4]:
+        return 'A'
+    elif arr[4] < brr[4]:
+        return 'B'
+    else:
+        if arr[3] > brr[3]:
+            return 'A'
+        elif arr[3] < brr[3]:
+            return 'B'
+        else:
+            if arr[2] > brr[2]:
+                return 'A'
+            elif arr[2] < brr[2]:
+                return 'B'
+            else:
+                if arr[1] > brr[1]:
+                    return 'A'
+                elif arr[1] < brr[1]:
+                    return 'B'
+                else:
+                    return 'D'
+
+for arr1, brr1 in zip(A, B):
+    print(solution(arr1, brr1))
