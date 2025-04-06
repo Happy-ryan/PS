5721eba08a7ffcd0a822a69e1485c86b3b8fa90b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-04-06 13:26:34 +0900

    [Gold IV] Title: 여행 가자, Time: 108 ms, Memory: 111064 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,31 +1,47 @@
-N = int(input())
-M = int(input())
-graph = [list(map(int,input().split())) for row in range(N)]
-plan = list(map(int,input().split()))
+n = int(input())
+m = int(input())
+cites = [list(map(int, input().split())) for _ in range(n)]
+plan = list(map(int, input().split()))
 
-par = [i for i in range(N+1)]
+def solution(n, cites, plan):
+    
+    par = [-1] * (n + 1)
+    sizes = [-1] * (n + 1)
+    
+    def find(x):
+        if par[x] == -1:
+            return x
+        
+        root = find(par[x])
+        par[x] = root
+        
+        return root
+    
+    def union(x, y):
+        x = find(x)
+        y = find(y)
+        
+        if x == y:
+            return False
+        
+        par[y] = x
+        sizes[x] += sizes[y]
+        
+        return True
+    
+    # cities는 인접행렬을 의미함.
+    for i in range(n):
+        for j in range(n):
+            if cites[i][j] == 1:
+                union(i, j)
+    
+    ans = set()
+    for p in plan:
+        # 1base -> 0base
+        ans.add(find(p - 1))
+        
+    if len(ans) == 1:
+        return 'YES'
+    return 'NO'
 
-def find(x):
-    if par[x] != x:
-        par[x] = find(par[x])
-        return par[x]
-    return x
-
-def union(a,b):
-    pa = find(a)
-    pb = find(b)
-    par[pa] = pb
-
-for r in range(N):
-    for c in range(N):
-        if graph[r][c] == 1: # graph - 0 base
-            union(r+1,c+1) # par - 1 base
-
-res = set()
-for i in plan:
-    res.add(find(i))
-
-if len(res) == 1:
-    print('YES')
-else:
-    print('NO')
+print(solution(n, cites, plan))
