3a99cdb919ae7ef3bfc3afa155282073308eef8a
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-04-06 17:29:29 +0900

    [Silver I] Title: 문자열 교환, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.py
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.py
@@ -5,13 +5,22 @@ def solution(s):
     w = s.count('a') # window 안에 a만 존재 = a의 총 갯수 = window 크기
     # 문자열 원형
     s = s + s
-    
-    inf = int(1e18)
-    cnt = inf
-    for i in range(n):
-        p_b = s[i:i + w].count('b') # b -> a로 교화
 
-        cnt = min(cnt, p_b)
+    # 1. 초기세팅
+    pb = s[0:0 + w].count('b')
+    cnt = int(1e18)
+    
+    for i in range(n):
+        # 2. 조건 확인
+        cnt = min(cnt, pb)
+        
+        # 3. 기존 칸 삭제
+        if s[i] == 'b':
+            pb -= 1
+        # 4. 다음 칸 준비
+        if s[i + w] == 'b':
+            pb += 1
+        
     return cnt
 
 print(solution(s))
