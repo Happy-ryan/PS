008a2e78c0f2fbbe98cbde72695c5db368a710b8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-04-06 10:48:11 +0900

    [Gold V] Title: 집합의 표현, Time: 516 ms, Memory: 142880 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,28 +1,42 @@
-import sys
-sys.setrecursionlimit(10**5)
+n, m = map(int, input().split())
+cmds = [list(map(int, input().split())) for _ in range(m)]
 
-n,m = map(int,input().split())
-
-parent = [i for i in range(n+1)]
-
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-        return parent[x]
-    return x
-
-
-def union(a,b):
-    pa = find(a)
-    pb = find(b)
-    parent[pa] = pb
-
-for _ in range(m):
-    c,a,b = map(int,input().split())
-    if c == 0:
-        union(a,b)
-    else:
-        if find(a) == find(b):
-            print('YES')
+def solution(n, m, cmds):
+    
+    par = [-1] * (n + 1)
+    sizes = [-1] * (n + 1)
+    
+    # root 찾기
+    def find(x):
+        # par[x]가 -1 이라는 것을 par[x] 가 root
+        if par[x] == -1:
+            return x
+        # par[x]가 root가 아니면 par[x]의 root를 찾아야함
+        root = find(par[x])
+        par[x] = root # par[x]의 부모를 찾은 후에 넣어줌
+        
+        return root
+    
+    # 합
+    def union(x, y):
+        x = find(x) # x의 root
+        y = find(y) # y의 root
+        if x == y: # 이미 같은 부모 -> union 불필요
+            return False
+        # 부모가 다르면 한쪽에 붙여야함
+        # y를 x 그룹에 넣으려고함 -> x그룹의 사이즈는 y그룹의 사이즈만큼 증가
+        par[y] = x
+        sizes[x] += sizes[y]
+        return True # 잘 결합
+    
+    for cmd, a, b in cmds:
+        if cmd == 0:
+            union(a, b)
         else:
-            print('NO')
+            if find(a) == find(b):
+                print('YES')
+            else:
+                print('NO')
+    
+                
+solution(n, m, cmds)
