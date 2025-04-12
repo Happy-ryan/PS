1bc07d2336b4b1acf11eaa0b4483a303aa1d5e2f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-04-12 22:32:28 +0900

    [Gold V] Title: 문자열 게임 2, Time: 288 ms, Memory: 114704 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -6,6 +6,7 @@ def solution(w, k):
     # k개 미만 문자는 제외.
     # 시간복잡도 O(len(w))
     for i in w:
+        # dic 쓸 때 pop 쓰면 key 사라질 수 있음..매우 주의!!
         if i in dic and dic[i] < k:
             dic.pop(i)
     
@@ -16,45 +17,24 @@ def solution(w, k):
             if i == key:
                 idxs.append(idx)
                 
-        val = len(w) + 1
-        for i in range(len(idxs) - k + 1):
-            # print("1 :", idxs[i + k])
-            # print("2 :", idxs[i])
-            val = min(val, idxs[i + k - 1] - idxs[i] + 1)
+        min_val, max_val = len(w), -len(w)
+        for i in range(k - 1, len(idxs)):
+            l, r = i - k + 1, i
+            min_val = min(min_val, idxs[r] - idxs[l] + 1)
+            max_val = max(max_val, idxs[r] - idxs[l] + 1)
             
-        return val
-    
-    def find_word4(key):
-        idxs = []
-        for idx, i in enumerate(w):
-            if i == key:
-                idxs.append(idx)
-                
-        val = 0
-        for i in range(len(idxs) - k + 1):
-            # print("1 :", idxs[i + k])
-            # print("2 :", idxs[i])
-            if w[idxs[i + k - 1]] == w[idxs[i]]:
-                val = max(val, idxs[i + k - 1] - idxs[i] + 1)
-            
-        return val
+        return min_val, max_val
     
     
     inf = int(1e18)        
     max_val, min_val = -inf, inf
-    # print(dic)
     for key in dic:
-        # print("key:", key)
-        val1 = find_word3(key)
-        val2 = find_word4(key)
-        # print('val1', val1)
-        # print('val2', val2)
-        max_val = max(max_val, val2)
-        min_val = min(min_val, val1)
-        # print("====")
+        val1, val2 = find_word3(key)
+        max_val = max(max_val, val1, val2)
+        min_val = min(min_val, val1, val2)
         
     if max_val == -inf and min_val == inf:
-        return -1
+        return [-1]
     return [min_val, max_val]
 
     
@@ -63,7 +43,4 @@ for _ in range(t):
     w = input()
     k = int(input())
     ans = solution(w, k)
-    if ans == -1:
-        print(ans)
-    else:
-        print(*ans)
+    print(*ans)
