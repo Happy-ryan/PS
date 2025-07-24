980671bfb2d7e0f352b09b1dbc0af08996881816
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-25 05:50:51 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,4 +1,33 @@
 -- 코드를 입력하세요
+# 2022년 3월 / 온.오프라인 판매날짜, 상품id, 유저id, 판매량
+# 오파라인 판매데이터의 경우 userid = null
+# 판매일 기준 오름차순 / 상푸id 오름차순 / 유저id 오름차순
+# 온라인과 오프라인 테이블을 합치는 것이 포인트...
+with tmp_union as (
+    select date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE,
+            PRODUCT_ID,
+            USER_ID,
+            sum(SALES_AMOUNT) as SALES_AMOUNT
+    from ONLINE_SALE
+    where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3
+    group by PRODUCT_ID
+    
+    union all
+    
+    select date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE,
+            PRODUCT_ID,
+            null as USER_ID,
+            sum(SALES_AMOUNT) as SALES_AMOUNT
+    from OFFLINE_SALE
+    where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3
+    group by PRODUCT_ID
+)
+
+select *
+from tmp_union
+order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc;
+
+
 # 2022년 3월
 # 오프라인 / 온라인 상품의 판대 데이터
 # 판매 날짜, 상품id, 유저id, 판매량
