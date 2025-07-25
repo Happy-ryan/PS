ff800c9386cc9ecf3e9a826834d4d717f23f4cc4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-26 08:56:13 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -69,15 +69,6 @@ from FOOD_PRODUCT
 where CATEGORY regexp '과자|국|김치|식용유'
 group by CATEGORY;
 
-
-
-
-
-
-
-
-
-
 # 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름 조회
 # 식품분류 - 과자 / 국 / 김치 / 식용유인 경우만 출력
 # 식품 가격을 기준으로 내림차순
@@ -93,3 +84,53 @@ inner join FOOD_PRODUCT as R on T.CATEGORY = R.CATEGORY
 where T.MAX_PRICE = R.PRICE
 order by T.MAX_PRICE desc;
 
+# row_number()사용해보기
+with tmp as (
+    select CATEGORY, PRICE, PRODUCT_NAME, 
+            row_number() over (partition by CATEGORY order by PRICE desc) as rn
+    from FOOD_PRODUCT
+    where CATEGORY regexp '식용유|과자|국|김치'
+)
+
+select CATEGORY, PRICE, PRODUCT_NAME
+from tmp
+where rn = 1
+order by PRICE desc;
+
+# 각 카테고리별 최대값 가져오기 = substring_index + group_concat
+# substring_index(group_concat(P.PRODUCT_NAME order by P.PRICE desc), ',', 1)
+select P.CATEGORY, MAX(P.PRICE) as MAX_PRICE, 
+       substring_index(group_concat(P.PRODUCT_NAME ORDER BY P.PRICE DESC), ',', 1)
+       AS PRODUCT_NAME
+from FOOD_PRODUCT P
+where P.CATEGORY in ('과자', '국', '김치', '식용유')
+group by P.CATEGORY
+order by P.PRICE desc;
+
+select CATEGORY,
+       substring_index(group_concat(PRICE order by PRICE desc), ',', 1) as MAX_PRICE,
+       substring_index(group_concat(PRODUCT_NAME order by PRICE desc), ',' , 1) as PRODUCT_NAME
+from FOOD_PRODUCT
+where CATEGORY in ('과자', '국', '김치', '식용유')
+group by CATEGORY
+order by cast(MAX_PRICE as UNSIGNED) desc;
+
+
+# 식품별로 가격기 제일 비싼 식품의 분류, 가겨그 이름을 조회하는 sql
+# 식품분류가 과자 국 김치 식용유만 출력
+# 식품 가격 기준 내림차순
+
+# 1) 각 식품에서 가장 비싼 가격 정렬하기
+# id까지 같이 그룹바이하면 아이디별 카테고리별 최고가가 나오니까 식용유에서도 최고가가 다양하게 나올 것!
+with tmp as (
+    select max(PRICE) as max_price, CATEGORY
+    from FOOD_PRODUCT
+    where CATEGORY in ('식용유', '과자', '국', '김치')
+    group by CATEGORY
+    
+)
+
+select P.CATEGORY, T.MAX_PRICE, P.PRODUCT_NAME
+from tmp as T
+inner join FOOD_PRODUCT as P on (P.CATEGORY = T.CATEGORY and P.PRICE = T.max_price)
+order by max_price desc;
