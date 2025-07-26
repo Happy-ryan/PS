c1e25182ba82f09ee5c287d042055781e311768f
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-26 11:22:39 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,16 +1,38 @@
--- 코드를 작성해주세요
--- 평균길이 33cm 이상 > 물고기 종류별로 구분 > 잡은수, 최대길이, 물고기의 종류를 출력
--- 물고기의 종류에 대해서 오름차순
--- 10cm이하 물고기들은 10cm로 취급
+# -- 코드를 작성해주세요
+# -- 평균길이 33cm 이상 > 물고기 종류별로 구분 > 잡은수, 최대길이, 물고기의 종류를 출력
+# -- 물고기의 종류에 대해서 오름차순
+# -- 10cm이하 물고기들은 10cm로 취급
 
--- 1. null이거나 10cm이하의 길이들은 10으로 변경해줘야한다.
+# -- 1. null이거나 10cm이하의 길이들은 10으로 변경해줘야한다.
+# -- if문을 썻지만 ifnull이 더 합리적이다.
+# -- 왜냐하면 10cm이하의 경우네는 null이기 때문에 10cm이하는 기록되지 않음.
+# with tmp as (
+#     select ID, FISH_TYPE, if(LENGTH <= 10 or LENGTH is null, 10, LENGTH) as LENGTH
+#     from FISH_INFO
+# )
+
+# -- group by 쓰고 집계함수 조건 판단할 때는 having절 사용한다.
+# select count(ID) as FISH_COUNT, max(LENGTH) as MAX_LENGTH, FISH_TYPE
+# from tmp
+# group by FISH_TYPE
+# having avg(LENGTH) >= 33
+# order by FISH_TYPE;
+
+
+
+
+
+
+
+# 평균길이 33cm 이상 물고기 종류별로 분류 잡은 수, 최대길이, 물고기 종류 출력
+# 종류 오름차순 / 10cm이하는 10cm로 
 with tmp as (
-    select ID, FISH_TYPE, if(LENGTH <= 10 or LENGTH is null, 10, LENGTH) as LENGTH
+    select count(ID) as FISH_COUNT, max(LENGTH) as max_length, FISH_TYPE
     from FISH_INFO
+    group by FISH_TYPE
+    having avg(if(LENGTH <= 10 or LENGTH is null, 10, LENGTH)) >= 33
 )
 
-select count(ID) as FISH_COUNT, max(LENGTH) as MAX_LENGTH, FISH_TYPE
+select *
 from tmp
-group by FISH_TYPE
-having avg(LENGTH) >= 33
-order by FISH_TYPE;
+order by FISH_TYPE asc;
