f047bc1f486d0b5b170fba26e6ea63d871608f15
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-28 22:08:16 +0900

    [Silver II] Title: 배열 놀이, Time: 1044 ms, Memory: 217980 KB -BaekjoonHub

--- 백준/Silver/17123. 배열 놀이/배열 놀이.py
+++ 백준/Silver/17123. 배열 놀이/배열 놀이.py
@@ -6,23 +6,17 @@ def solution(N, M, board, qs):
     # 1) 증감 event 기록
     for q in qs:
         r1, c1, r2, c2, v = q # 1base
-        event[r1 - 1][c1 - 1] += v
-        event[r1 - 1][c2] += -v
-        event[r2][c1 - 1] += -v
-        event[r2][c2] += v
-        # for row in event:
-        #     print(row)
-        # print("======")
+        event[r1 - 1][c1 - 1] += v # event 발생
+        event[r1 - 1][c2] += -v # event 미발생
+        event[r2][c1 - 1] += -v # event 미발생
+        event[r2][c2] += v # event 미발생 지역이 겹쳐, 2번 중복으로 - 발생해서 +로 보정해야함!!!
+
     
     # 2) 복구 - 누적합
     psum = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
     for i in range(N):
         for j in range(N):
             psum[i + 1][j + 1] = event[i][j] + psum[i + 1][j] + psum[i][j + 1] - psum[i][j]
-    
-    # for row in psum:
-    #     print(row)
-    # print("======")
     
     for i in range(N):
         for j in range(N):
@@ -46,9 +40,8 @@ def solution(N, M, board, qs):
     
     print(*row)
     print(*col)
-            
-    
-    
+
+
 for _ in range(t):
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
