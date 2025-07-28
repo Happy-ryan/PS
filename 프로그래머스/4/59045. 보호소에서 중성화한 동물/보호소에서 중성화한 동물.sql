f608bd5d693a77e75d4a0c981443e389279602d9
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-28 12:09:04 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -12,3 +12,16 @@ from ANIMAL_OUTS O
 inner join temp T on O.ANIMAL_ID = T.ANIMAL_ID	
 where O.SEX_UPON_OUTCOME like 'Spayed%' or O.SEX_UPON_OUTCOME like 'Neutered%'
 order by O.ANIMAL_ID;
+
+
+# 보호소 입소 당시 - 중성화안했지만 / 보호소 출소 당시 - 중성화
+# 아이디 / 종 / 이름 
+# 아이디순으로 조회
+
+select I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+from ANIMAL_INS as I
+inner join ANIMAL_OUTS as O on I.ANIMAL_ID = O.ANIMAL_ID
+where I.SEX_UPON_INTAKE like 'Intact%' and (
+    O.SEX_UPON_OUTCOME  like 'Neutered%' or O.SEX_UPON_OUTCOME  like 'Spayed%'
+)
+order by I.ANIMAL_ID asc;
