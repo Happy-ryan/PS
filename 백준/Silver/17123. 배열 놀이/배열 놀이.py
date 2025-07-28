f061f17c4337b523bc1d04236ce3e54cbe9a7e69
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-28 22:04:22 +0900

    [Silver II] Title: 배열 놀이, Time: 1272 ms, Memory: 217988 KB -BaekjoonHub

---
+++ 백준/Silver/17123. 배열 놀이/배열 놀이.py
@@ -0,0 +1,56 @@
+t = int(input())
+
+def solution(N, M, board, qs):
+    event = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+    
+    # 1) 증감 event 기록
+    for q in qs:
+        r1, c1, r2, c2, v = q # 1base
+        event[r1 - 1][c1 - 1] += v
+        event[r1 - 1][c2] += -v
+        event[r2][c1 - 1] += -v
+        event[r2][c2] += v
+        # for row in event:
+        #     print(row)
+        # print("======")
+    
+    # 2) 복구 - 누적합
+    psum = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+    for i in range(N):
+        for j in range(N):
+            psum[i + 1][j + 1] = event[i][j] + psum[i + 1][j] + psum[i][j + 1] - psum[i][j]
+    
+    # for row in psum:
+    #     print(row)
+    # print("======")
+    
+    for i in range(N):
+        for j in range(N):
+            board[i][j] += psum[i + 1][j + 1]
+    
+    row, col = [], []
+    
+    def cal_row():
+        for ro in board:
+            row.append(sum(ro))
+    
+    def cal_col():
+        for j in range(N):
+            val = 0
+            for i in range(N):
+                val += board[i][j]
+            col.append(val)
+    
+    cal_row()
+    cal_col()
+    
+    print(*row)
+    print(*col)
+            
+    
+    
+for _ in range(t):
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    qs = [list(map(int, input().split())) for _ in range(M)]
+    solution(N, M, board, qs)
