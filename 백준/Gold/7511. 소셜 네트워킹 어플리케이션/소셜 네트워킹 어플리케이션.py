47aa436e2746d13281979ac5d7035e4927e6598e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-07-30 10:33:23 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 796 ms, Memory: 194684 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
@@ -1,50 +1,51 @@
-from collections import defaultdict
-
 t = int(input())
 
-def solution(n, friends, m, qs):
+def solution(t, n, k, infos, m, qs):
+    # 친구 -> 그룹
+    # 연결 파악 -> 같은 그룹인지 확인
+    par = [-1] * (n + 1)
+    sizes = [-1] * (n + 1)
     
-    par = [-1] * (n)
-    
-    # root 찾기
     def find(x):
         if par[x] == -1:
             return x
-        par[x] = find(par[x])
-        return par[x]
+        
+        root = find(par[x])
+        par[x] = root
+        
+        return root
     
-    def merge(x, y):
-        x = find(x)
-        y = find(y)
+    def union(x, y):
+        x, y = find(x), find(y)
+        
         if x == y:
             return False
-        par[y] = x
-        return True
-    
-    for a, b in friends:
-        merge(a, b)
         
-    dic = defaultdict(list)
-    for idx, node in enumerate(par):
-        # 해당 노드의 부모를 찾고 그 부모를 가진 그룹 만들기
-        root = find(idx)
-        dic[root].append(idx)
+        if sizes[x] < sizes[y]:
+            x, y,  = y, x
+            
+        par[y] = x
+        sizes[x] += sizes[y]
+        
     
-    # 같은 그룹 파악하는데 용이
-    for a, b in qs:
-        a = find(a)
-        b = find(b)
-        if a == b:
+    print(f"Scenario {t}:")
+    for info in infos:
+        a, b = info
+        union(a, b)
+        
+    for q in qs:
+        v, w = q
+        if find(v) == find(w):
             print(1)
         else:
             print(0)
-    
+            
+            
 for i in range(t):
     n = int(input())
     k = int(input())
-    friends = [list(map(int, input().split())) for _ in range(k)]
+    infos = [list(map(int, input().split())) for _ in range(k)]
     m = int(input())
     qs = [list(map(int, input().split())) for _ in range(m)]
-    print(f"Scenario {i + 1}:")
-    solution(n, friends, m, qs)
+    solution(i +1, n, k, infos, m, qs)
     print()
