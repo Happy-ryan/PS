8b1815c381f574c5b35f9cfbf8a7f0e64082ffb3
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-08-08 03:28:23 +0900

    [level 0] Title: 문자열로 변환, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int n) {
+        // int -> String 변경 : Integer.toString()
+        // int -> String 변경 : String.valueOf
+        // return Integer.toString(n);
+        return String.valueOf(n);
+    }
+}
