620f848e2de0a945b58a5c5fbf48a0aa2adb2fcb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-08-08 03:17:54 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 2.13 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -2,17 +2,17 @@ class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
         String str = "";
+        StringBuilder sb = new StringBuilder ();
         for (char x : myString.toCharArray()) {
             if (x == 'A') {
                 str += 'B';
+                sb.append('B');
             }
             else {
                 str += 'A';
+                sb.append('A');
             }
         }
-        if (str.contains(pat)) {
-            return 1;
-        }
-        return 0;
+        return sb.toString().contains(pat) ? 1 : 0;
     }
 }
