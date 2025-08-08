2ee4965411586dbb2d631eb7eeeea5c1367dd7c8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-08-09 01:54:34 +0900

    [Silver II] Title: 소수 부분 수열, Time: 456 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/6884. 소수 부분 수열/소수 부분 수열.java
+++ 백준/Silver/6884. 소수 부분 수열/소수 부분 수열.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 
@@ -20,39 +21,72 @@ class Solution {
         int minStart = -1;
 
         // 길이는 최소 2
+//        for (int w = 2; w < size + 1; w++) {
+//            // python 방식 사용하기 어려우니까..break 두 번 걸리하는 조건을 잘 생각해야함!
+//            boolean found = false;
+//            for (int i = 0; i < size - w + 1; i++) {
+//                int val = psum[i + w] - psum[i];
+//                if (isPrime(val) == 1) {
+//                    minLength = w;
+//                    minStart = i;
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if (found) {
+//                break;
+//            }
+//        }
+
+        int inf = Integer.MAX_VALUE;
+        int[] val = {inf, inf};
         for (int w = 2; w < size + 1; w++) {
-            boolean found = false;
             for (int i = 0; i < size - w + 1; i++) {
-                int val = psum[i + w] - psum[i];
-                if (isPrime(val) == 1) {
-                    minLength = w;
-                    minStart = i;
-                    found = true;
-                    break;
+                int a = psum[i + w] - psum[i];
+                if (isPrime(a) == 1) {
+                    int[] cur = {w, i};
+                    if (compareArrays(cur, val) < 0) {
+                        val = cur;
+                    }
                 }
-            }
-            if (found) {
-                break;
             }
         }
 
 //        System.out.println(minLength);
 //        System.out.println(minStart);
 
-        if (minLength == Integer.MAX_VALUE) {
+        if (val[0] == inf) {
             return "This sequence is anti-primed.";
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append("Shortest primed subsequence is length ");
-        sb.append(minLength).append(": ");
+        sb.append("Shortest primed subsequence is length ").append(val[0]).append(": ");
 
-        for (int i = minStart; i < minStart + minLength; i++) {
-            sb.append(nums[i + 1]);
-            sb.append(" ");
+        for (int i = val[1]; i < val[1] + val[0]; i++) {
+            sb.append(nums[i + 1]).append(" ");
         }
 
+//        if (minLength == Integer.MAX_VALUE) {
+//            return "This sequence is anti-primed.";
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("Shortest primed subsequence is length ");
+//        sb.append(minLength).append(": ");
+//
+//        for (int i = minStart; i < minStart + minLength; i++) {
+//            sb.append(nums[i + 1]);
+//            sb.append(" ");
+//        }
+
         return sb.toString();
+    }
+
+    public static int compareArrays(int[] a, int[] b) {
+        if (a[0] != b[0]) {
+            return Integer.compare(a[0], b[0]);
+        }
+        return Integer.compare(a[1], b[1]);
     }
 
     public static int isPrime(int n) {
