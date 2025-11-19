66b5ad056e0183e66248af4f403966554c09be22
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-11-19 21:59:41 +0900

    [Bronze IV] Title: 추석은 언제나 좋아, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34750. 추석은 언제나 좋아/추석은 언제나 좋아.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+if N >= 1000000:
+    give = N * 20 // 100
+elif N >= 500000:
+    give = N * 15 // 100
+elif N >= 100000:
+    give = N * 10 // 100
+else:
+    give = N * 5 // 100
+
+keep = N - give
+
+print(give, keep)
