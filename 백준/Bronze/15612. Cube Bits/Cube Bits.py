ec613647862b21ee4cd80546589cbed5d54d0f44
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-12-02 12:39:02 +0900

    [Bronze II] Title: Cube Bits, Time: 1156 ms, Memory: 120568 KB -BaekjoonHub

--- 백준/Bronze/15612. Cube Bits/Cube Bits.py
+++ 백준/Bronze/15612. Cube Bits/Cube Bits.py
@@ -1,16 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def conv(n):
-    if n == 0:
-        return "0"
-    res = []
-    while n > 0:
-        n, mod = divmod(n, 3)
-        res.append(str(mod))
-    return ''.join(res[::-1])
-
 t = int(input())
+
+def conv(n, m):
+    if n == 0:
+        return 0
+    res = ''
+    conv = '0123456789abcdef'
+    while n > 0:
+        n, mod = divmod(n, m)
+        res += conv[mod]
+    return res[::-1]
+
 for _ in range(t):
     n = int(input())
-    print(conv(n))
+    print(conv(n, 3))
