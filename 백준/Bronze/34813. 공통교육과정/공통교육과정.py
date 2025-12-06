358dbcdef4d8d903e45d3105fa37f0a5e3e33d46
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-12-07 00:38:23 +0900

    [Bronze V] Title: 공통교육과정, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,12 @@
+s = input().strip()
+
+first = s[0]
+
+if first == 'F':
+    print("Foundation")
+elif first == 'C':
+    print("Claves")
+elif first == 'V':
+    print("Veritas")
+elif first == 'E':
+    print("Exploration")
