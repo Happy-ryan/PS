1f57dfc6cdc1481489af2e0fac49dd27f10b92f3
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-12-26 05:01:51 +0900

    [Silver V] Title: 최대 상승, Time: 140 ms, Memory: 143808 KB -BaekjoonHub

---
+++ 백준/Silver/25644. 최대 상승/최대 상승.py
@@ -0,0 +1,15 @@
+n = int(input())
+prices = list(map(int, input().split()))
+
+def solution(n, prices):
+    
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices:
+        max_profit = max(max_profit, price - min_price)
+        min_price = min(min_price, price)
+
+    return max_profit 
+    
+print(solution(n, prices))
