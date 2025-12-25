e11e3ce1c35de1503d21c1e7f743eff6fecc2bf3
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-12-25 16:20:36 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 115060 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,77 +1,76 @@
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
-# 공기 중에서 치즈가 모두 녹아 없어지는 데 걸리는 시간
-# 모두 녹기 한 시간 전에 남아 있는 치즈조각이 놓여있는 칸의 수
-
-# 1. 공기의 영역을 찾기 -> bfs
-# 2. 공기 영역 찾은 후 겉에 있는 치즈 -> 공기로 변경하기
-
 from collections import deque
 
-
 def solution(n, m, board):
-
+    # 치즈 구멍 / 치즈 가장자리 구분 -> 가장자리가 녹음
+    # bfs를 통해 치즈 구멍인지 그냥 비어있는 곳인지 판단 > 다른 그룹에 속함
+    # (0, 0) & bfs > 전부 그냥 비어있는 곳 > 근접 치즈 녹이기
+    
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
-
+    
     def in_range(r, c):
         return 0 <= r < n and 0 <= c < m
-
-    def find_air():
-        airs = []
-        visited = [[False for _ in range(m)] for _ in range(n)]
-
+    
+    def bfs(r, c, mode):
+        
+        if mode == 'c':
+            H = 1
+        else:
+            H = 0
+        
+        queue = [[False for _ in range(m)] for _ in range(n)]
+        
         dq = deque([])
-
-        dq.append((0, 0))
-        airs.append((0, 0))
-        visited[0][0] = True
-
+        
+        dq.append((r, c))
+        queue[r][c] = True
+        grids = [(r, c)]
+        
         while dq:
             cr, cc = dq.popleft()
             for k in range(4):
                 nr = cr + dr[k]
                 nc = cc + dc[k]
-                if in_range(nr, nc) and not visited[nr][nc] and board[nr][nc] == 0:
-                    dq.append((nr, nc))
-                    airs.append((nr, nc))
-                    visited[nr][nc] = True
-
-        return airs
-
-    def change_cheese(airs):
-        for r, c in airs:
+                if  in_range(nr, nc) and\
+                    board[nr][nc] == H and\
+                    not queue[nr][nc]:
+                        
+                        dq.append((nr, nc))
+                        queue[nr][nc] = True
+                        grids.append((nr, nc))
+                        
+        return grids
+    
+    # 치즈를 동시에 녹여야함. 하나씩이 아니라.
+    def melt_effect(airs):
+        
+        melts = set() # 치즈 좌표 중복 제거
+        
+        for air in airs:
+            r, c = air
             for k in range(4):
                 nr = r + dr[k]
                 nc = c + dc[k]
-                if in_range(nr, nc) and board[nr][nc] == 1:
-                    board[nr][nc] = 0
-
-    def count_cheese():
-        cnt = 0
-        for r in range(n):
-            for c in range(m):
-                if board[r][c] == 1:
-                    cnt += 1
-
-        return cnt
-
-    t = 0
-    ans = 0
-    while True:
-        final_cheese = count_cheese()
-        if final_cheese == 0:
-            break
-
-        airs = find_air()
-        change_cheese(airs)
-        ans = final_cheese
-
-        t += 1
+                if in_range(nr, nc) and\
+                    board[nr][nc] == 1:
+                        melts.add((nr, nc))
         
-    print(t)
-    print(ans)
-
-
-solution(n, m, board)
+        for melt in melts:
+            r, c = melt
+            board[r][c] = 0
+        
+        return len(melts) # 녹는 치즈의 수
+    
+    t = 0
+    while True:
+        airs = bfs(0, 0, 'a')
+        if len(airs) == n * m:
+            return f"{t}\n{melts}"
+        t += 1
+        melts = melt_effect(airs)
+        # print(f"시간: {t}, 녹는 치즈의 수: {melts}")
+        
+print(solution(n, m, board))
