e89814cfc06b6370372ca7fc50715d0e8ede7045
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2025-12-27 04:14:29 +0900

    [Gold V] Title: 트리, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,55 +1,50 @@
-n = int(input())
+N = int(input())
 nodes = list(map(int, input().split()))
 del_node = int(input())
 
-def solution(n, nodes, del_node):
-    
-    adj = [[] for _ in range(n)]
-    # print(adj)
-    # print(nodes)
-    for idx, node in enumerate(nodes):
-        if node != -1:
-            adj[node].append(idx)
-            
-    # 리프노드 = ajd에서 []인 것들! > 자식이 없기 때문!
-    leaf_node = []
-    for idx, row in enumerate(adj):
-        if not len(row):
-            leaf_node.append(idx)
-
-    # print(leaf_node)
-    
-    visitd = [False] * (n)
-    def dfs(node):
-        if visitd[node]:
-            return
-        visitd[node] = True
+def solution(N, nodes, del_node):
+    global cnt
+    cnt = 0
+    adj = [[] for _ in range(N)]
+    for idx, num in enumerate(nodes):
+        if num == -1 or idx == del_node:
+            continue
+        adj[num].append(idx) # idx가 노드번호
         
-        for x in adj[node]:
-            dfs(x)
+    # 삭제노드 = 방문 물가 > visited 기록
+    visited = [False for _ in range(N)]
+    visited[del_node] = True
     
+    leaf_nodes = []
+    def dfs(cur, lv):
+        global cnt
+        # cnt = 0
+        visited[cur] = True # 방문체크
+        
+        # 리프노드 체크
+        if len(adj[cur]) == 0:
+            cnt += 1
+            leaf_nodes.append(cur)
+        # print(f"방문 :{cur}, visited: {visited}, lv :{lv}, cnt: {cnt}")
+        for nxt in adj[cur]:
+            if not visited[nxt]:
+                dfs(nxt, lv + 1)
+                # cnt += 1
+        # # cnt 똑같이 0이라는건 방문한게 없다는 것 > 리프노드
+        # if cnt == 0: 
+        #     leaf_nodes.append(cur)
+
+    root = nodes.index(-1)
     
-    dfs(del_node)
-    
-    # print(visitd)
-    
-    # 방문한 노드 제외
-    cnt = len(leaf_node)
-    for leaf in leaf_node:
-        if visitd[leaf]:
-            cnt -= 1
-            
-    # 틀린 이유 : -1 노드가 두 개의 자식 노드가 있으면 부모 노드는 절대 자식노드 될 수 없음
-    # 문제는, 트리가 한 줄로 되어있을 때 자식이 죽어도 리프노드는 계속 새로 생성됨!
-    
-    if del_node == nodes.index(-1):
+    # print(adj, root)
+    if root == del_node:
         return 0
     
-    par = nodes[del_node] # 지운 node의 부모
-    par_size = len(adj[par]) # 부모 node 자식의 수
-    if par_size == 1:
-        cnt += 1
+    dfs(root, 0)
     
+    # print(leaf_nodes)
+    
+    # return len(leaf_nodes)
     return cnt
-        
-print(solution(n, nodes, del_node))
+    
+print(solution(N, nodes, del_node))
