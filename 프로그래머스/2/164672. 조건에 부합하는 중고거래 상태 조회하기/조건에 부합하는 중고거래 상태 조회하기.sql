a0ca2a1b702ac4138cfada2a472e4f54f09b1cf8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-01-01 10:25:46 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,4 +1,17 @@
 -- 코드를 입력하세요
+# 2022년 10월 5일 등록된 게시물 > 
+# 거래상태 SALE 판매중 ~ / 게시글ID기준 내림차순
+select BOARD_ID, WRITER_ID, TITLE, 
+        CASE 
+            when STATUS = 'SALE' then '판매중'
+            when STATUS = 'RESERVED' then '예약중'
+            else '거래완료'
+        END AS 'STATUS'
+from USED_GOODS_BOARD
+where year(CREATED_DATE) = 2022 and month(CREATED_DATE) = 10
+order by BOARD_ID desc;
+
+
 # 2022년 10월 5일 등록
 # 게시글ID,작성사ID,게시글 제목, 가격, 거래상태 조회
 # SALE이면 판매중 / RESERVED이면 예약중 / DONE이면 거래완료
@@ -26,4 +39,14 @@ select BOARD_ID, WRITER_ID, TITLE, PRICE,
         end as STATUS
 from USED_GOODS_BOARD
 where year(CREATED_DATE) = 2022 and month(CREATED_DATE) = 10 and day(CREATED_DATE) = 5
+order by BOARD_ID desc;
+
+select BOARD_ID, WRITER_ID, TITLE, PRICE,
+        case 
+            WHEN STATUS = 'SALE' then '판매중'
+            WHEN STATUS = 'RESERVED' then '예약중'
+            when STATUS = 'DONE' then '거래완료'
+        end as STAUS
+from USED_GOODS_BOARD
+where timestampdiff(day, '2022-10-05', CREATED_DATE) = 0
 order by BOARD_ID desc;
