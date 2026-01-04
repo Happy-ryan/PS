4922223145abbe23c14cc32757c4896c78aa79f6
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-01-04 16:17:49 +0900

    [Bronze III] Title: Fractification, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/19751. Fractification/Fractification.py
+++ 백준/Bronze/19751. Fractification/Fractification.py
@@ -1,21 +1,7 @@
-from fractions import Fraction
-from itertools import product, permutations
-
 a, b, c, d = map(int, input().split())
 
-# a / b + c / d <- 현재 a b c d에서 적절히 변경했을 때 두 분수의 합의 최솟값
-# 전체 경우의 수 4!
-inf = int(1e18)
-cnt = inf
-answer = (a, b, c, d)
-for row in permutations([a, b, c, d]):
-    a, b, c, d = row
-    P1 = Fraction(a, b)
-    P2 = Fraction(c, d)
-    P3 = P1 + P2
-    if cnt > P3:
-        cnt = P3
-        answer = (a, b, c, d)
-    
-# print(cnt)
-print(*answer)
+# 정렬
+nums = sorted([a, b, c, d])
+
+# 최적 배치: x1/x3 + x2/x4
+print(nums[0], nums[2], nums[1], nums[3])
