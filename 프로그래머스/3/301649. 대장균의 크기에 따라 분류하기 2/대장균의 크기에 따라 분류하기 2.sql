d6b8fdcbb0912263eed27014402b2eb449e5715b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-01-06 20:55:43 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -18,18 +18,35 @@
 # from tmp
 # order by ID asc;
 
-WITH tmp AS (
-    SELECT ID,
-        NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS NT
-    FROM ECOLI_DATA
+# WITH tmp AS (
+#     SELECT ID,
+#         NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS NT
+#     FROM ECOLI_DATA
+# )
+
+# SELECT ID,
+#     CASE NT
+#         WHEN 1 THEN 'CRITICAL'
+#         WHEN 2 THEN 'HIGH'
+#         WHEN 3 THEN 'MEDIUM'
+#         ELSE 'LOW'
+#     END AS COLONY_NAME
+# FROM tmp
+# ORDER BY ID ASC;
+
+# ntile() 과 percent_rank() 라는 윈도우함수 존재
+# 1.
+with tmp as (
+    select ID, percent_rank() over (order by SIZE_OF_COLONY desc) as RN
+    from ECOLI_DATA
 )
 
-SELECT ID,
-    CASE NT
-        WHEN 1 THEN 'CRITICAL'
-        WHEN 2 THEN 'HIGH'
-        WHEN 3 THEN 'MEDIUM'
-        ELSE 'LOW'
-    END AS COLONY_NAME
-FROM tmp
-ORDER BY ID ASC;
+select ID,
+    case
+        when RN <= 0.25 then 'CRITICAL'
+        when RN <= 0.5 then 'HIGH'
+        when RN <= 0.75 then 'MEDIUM'
+        else 'LOW'
+    end as COLONY_NAME
+from tmp
+order by ID asc;
