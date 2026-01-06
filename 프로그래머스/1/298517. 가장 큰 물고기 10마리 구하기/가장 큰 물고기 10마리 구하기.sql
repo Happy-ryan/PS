4efcb9e6714b05421132f1610b4102e3fc41365b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-01-06 17:55:27 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -17,10 +17,25 @@
 # order by LENGTH desc, ID asc;
 
 
+# select T.ID, T.LENGTH
+# from(
+#     select ID, LENGTH, row_number() over(order by LENGTH desc) as rn
+#     from FISH_INFO
+# ) as T
+# where rn between 1 and 10
+# order by T.LENGTH desc, T.ID asc;
+
+# group concat
+# select substring_index(group_concat(ID order by LENGTH desc, ID asc), ',', 1, 10) 
+# from FISH_INFO;
+
 select T.ID, T.LENGTH
-from(
-    select ID, LENGTH, row_number() over(order by LENGTH desc) as rn
+from (
+    select  ID,
+            LENGTH,
+            row_number() over(order by LENGTH desc) as rn
     from FISH_INFO
 ) as T
-where rn between 1 and 10
+where T.rn between 1 and 10
 order by T.LENGTH desc, T.ID asc;
+
