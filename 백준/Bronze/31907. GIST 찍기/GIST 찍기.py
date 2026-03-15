a231bd35db62e11974bdcb3111c6fb9aeb5a5dfb
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-16 00:44:32 +0900

    [Bronze III] Title: GIST 찍기, Time: 92 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31907. GIST 찍기/GIST 찍기.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for _ in range(n):
+    print("G" * n + '...' * n)
+
+for _ in range(n):
+    print('.' * n + 'I' * n + '.' * n + 'T' * n)
+    
+for _ in range(n):
+    print('..' * n + 'S' * n + '.' * n)
