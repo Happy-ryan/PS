1fbf9c6f8d79e905a84385c1a0fa10d590b84511
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-26 00:21:54 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1220 ms, Memory: 225272 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,62 +1,57 @@
-n, m = map(int, input().split())
-board = [list(input()) for _ in range(n)]
-
 from collections import deque
-def solution(n, m, board):
-    # 왜 bfs? 그래프 상에서 이동거리 최소 찾기
-    # 특이점
-        # 벽을 부순 것과 안부순 상태를 확인해서 이동!
-        # 벽의 상태에 대한 차원이 필요함. > 벽을 부수면 더 빨라질 가능성이 있기 때문
 
-    # 행, 열, 차원
-    inf = int(1e18)
+n, m = map(int, input().split())
+board = [list(map(int, input())) for _ in range(n)]
+
+def solution(n, m, board):
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
-    
-    in_queue = [[[False for _ in range(m)] for _ in range(n)] for _ in range(2)]
+    # 시작점(0, 0) ~ 도착점(n - 1, m - 1)
+    # 최단거리 
+    # 부서진 벽인지 아닌지 체크하는 차원 필요 - 벽이 부서지냐 안부서지냐
+    inf = int(1e18)
+    # 열 / 행 / 벽
+    visited = [[[False for _ in range(m)] for _ in range(n)] for _ in range(2)]
     dist = [[[inf for _ in range(m)] for _ in range(n)] for _ in range(2)]
-        
-    def in_range(r, c, k):
-        return 0 <= r < n and 0 <= c < m and 0 <= k < 2
     
-    def bfs(r, c, k):
-        dq = deque([])
+    def in_range(k, r, c):
+        return 0 <= k < 2 and 0 <= r < n and 0 <= c < m
+    
+    def bfs():
         
-        # (행, 열, 벽)
-        dq.append((r, c, k))
-        in_queue[k][r][c] = True
-        dist[k][r][c] = 1
+        dq = deque([])
+        # (벽, 행, 열)
+        dq.append((0, 0, 0))
+        visited[0][0][0] = True
+        dist[0][0][0] = 1
         
         while dq:
-            cr, cc, ck = dq.popleft()
+            ck, cr, cc = dq.popleft()
             for dir in range(4):
                 nr = cr + dr[dir]
                 nc = cc + dc[dir]
-                if in_range(nr, nc, ck) and not in_queue[ck][nr][nc] and board[nr][nc] == '0':
-                    dq.append((nr, nc, ck))
-                    in_queue[ck][nr][nc] = True
+                # 벽 없을 때
+                if in_range(ck, nr, nc) and not visited[ck][nr][nc] and board[nr][nc] == 0:
+                    dq.append((ck, nr, nc))
+                    visited[ck][nr][nc] = True
                     dist[ck][nr][nc] = dist[ck][cr][cc] + 1
-                
-                # (point) 벽 부수기  - 다음 칸에 벽이 있어야함(board[nr][nc] == 1)
+                    
+                # 벽 있을 때 / 벽은 1이다.
                 nk = ck + 1
-                if in_range(nr, nc, nk) and not in_queue[nk][nr][nc] and board[nr][nc] == '1':
-                    dq.append((nr, nc, nk))
-                    in_queue[nk][nr][nc] = True
+                if in_range(nk, nr, nc) and not visited[nk][nr][nc] and board[nr][nc] == 1:
+                    dq.append((nk, nr, nc))
+                    visited[nk][nr][nc] = True
                     dist[nk][nr][nc] = dist[ck][cr][cc] + 1
-        
-        return dist
-    
-    
-    dist = bfs(0, 0, 0)
+                    
+    bfs()  
     
     ans = inf
-    
-    for ver in range(2):
-        ans = min(dist[ver][n - 1][m - 1], ans)
-        
+    for i in range(2):
+        ans = min(ans, dist[i][-1][-1])
+
     if ans >= inf:
-        return -1
-    
-    return ans
+        ans = -1
         
+    return ans            
+    
 print(solution(n, m, board))
