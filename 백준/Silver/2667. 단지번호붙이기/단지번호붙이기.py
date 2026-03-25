a2dd76a559df032efa7bae09a6cb39a2b4b8e1fa
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-25 22:34:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,6 +1,7 @@
 n = int(input())
 board = [list(input()) for _ in range(n)]
 
+# bfs
 from collections import deque
 
 def solution(n, board):
@@ -8,39 +9,43 @@ def solution(n, board):
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
     
-    in_queue = [[False for _ in range(n)] for _ in range(n)]
-    
     def in_range(r, c):
         return 0 <= r < n and 0 <= c < n
     
+    in_queue = [[False for _ in range(n)] for _ in range(n)]
+    
     def bfs(r, c):
         dq = deque([])
+        
         dq.append((r, c))
         in_queue[r][c] = True
         cnt = 1
         
         while dq:
             cr, cc = dq.popleft()
-            for k in range(4):
+            for  k in range(4):
                 nr = cr + dr[k]
                 nc = cc + dc[k]
-                if in_range(nr, nc) and not in_queue[nr][nc] and board[nr][nc] == '1':
-                    dq.append((nr, nc))
-                    in_queue[nr][nc] = True
-                    cnt += 1
+                if in_range(nr, nc) and\
+                    not in_queue[nr][nc] and\
+                        board[nr][nc] == '1':
+                            dq.append((nr, nc))
+                            in_queue[nr][nc] = True
+                            cnt += 1
         return cnt
-    
+        
     cnts = []
-    for r in range(n):
-        for c in range(n):
-            if board[r][c] == '0' or in_queue[r][c]:
+    for i in range(n):
+        for j in range(n):
+            if in_queue[i][j] or board[i][j] == '0':
                 continue
-            cnt = bfs(r, c)
+            cnt = bfs(i, j)
             cnts.append(cnt)
-    
+            
     cnts.sort()
+    
     print(len(cnts))
-    for cnt in cnts:
-        print(cnt)
-
+    for c in cnts:
+        print(c)
+    
 solution(n, board)
