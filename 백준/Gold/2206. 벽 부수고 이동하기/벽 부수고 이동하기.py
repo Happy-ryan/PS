0f05c1db4f01e8860ed4bf081bd4280821fc6cc4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-26 00:21:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1108 ms, Memory: 223372 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,50 +1,58 @@
-from collections import deque
-
 n, m = map(int, input().split())
 board = [list(map(int, input())) for _ in range(n)]
 
+from collections import deque
+
+
 def solution(n, m, board):
+
+    inf = int(1e14)
     dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-    # 시작점(0, 0) ~ 도착점(n - 1, m - 1)
-    # 최단거리 
-    # 부서진 벽인지 아닌지 체크하는 차원 필요 - 벽이 부서지냐 안부서지냐
-    inf = int(1e18)
-    # 열 / 행 / 벽
+    dc = [0, 0, 1, -1]
+
     visited = [[[False for _ in range(m)] for _ in range(n)] for _ in range(2)]
     dist = [[[inf for _ in range(m)] for _ in range(n)] for _ in range(2)]
-    
-    def in_range(k, r, c):
-        return 0 <= k < 2 and 0 <= r < n and 0 <= c < m
-    
+
+    def in_range(r, c, k):
+        return 0 <= r < n and 0 <= c < m and 0 <= k < 2
+
     def bfs():
-        
+
         dq = deque([])
-        # (벽, 행, 열)
         dq.append((0, 0, 0))
+        # k, n, m
         visited[0][0][0] = True
         dist[0][0][0] = 1
-        
+
         while dq:
-            ck, cr, cc = dq.popleft()
-            for dir in range(4):
-                nr = cr + dr[dir]
-                nc = cc + dc[dir]
-                # 벽 없을 때
-                if in_range(ck, nr, nc) and not visited[ck][nr][nc] and board[nr][nc] == 0:
-                    dq.append((ck, nr, nc))
+            cr, cc, ck = dq.popleft()
+            for k in range(4):
+                nr = cr + dr[k]
+                nc = cc + dc[k]
+                # 벽 안 부심.
+                if (
+                    in_range(nr, nc, ck)
+                    and not visited[ck][nr][nc]
+                    and board[nr][nc] == 0
+                ):
+                    dq.append((nr, nc, ck))
                     visited[ck][nr][nc] = True
                     dist[ck][nr][nc] = dist[ck][cr][cc] + 1
-                    
-                # 벽 있을 때 / 벽은 1이다.
+
+                # 벽 부심 > board에서는 벽이 선택되어야함.
+                # board = 1
                 nk = ck + 1
-                if in_range(nk, nr, nc) and not visited[nk][nr][nc] and board[nr][nc] == 1:
-                    dq.append((nk, nr, nc))
+                if (
+                    in_range(nr, nc, nk)
+                    and not visited[nk][nr][nc]
+                    and board[nr][nc] == 1
+                ):
+                    dq.append((nr, nc, nk))
                     visited[nk][nr][nc] = True
                     dist[nk][nr][nc] = dist[ck][cr][cc] + 1
-                    
-    bfs()  
-    
+
+    bfs()
+
     ans = inf
     for i in range(2):
         ans = min(ans, dist[i][-1][-1])
@@ -52,6 +60,6 @@ def solution(n, m, board):
     if ans >= inf:
         ans = -1
         
-    return ans            
-    
+    return ans
+
 print(solution(n, m, board))
