ade61ad985b17e32b00e2383c50f231ee607ddb1
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-25 23:32:06 +0900

    [Gold IV] Title: 빙산, Time: 708 ms, Memory: 191488 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,97 +1,89 @@
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
-from collections import deque
+from collections import deque, defaultdict
 
 def solution(n, m, board):
-    
-    # 빙하 / 물 <- 빙하가 주변의 물에 영향을 받는다는 것..
-    # 한번에 영향을 받아 동시에 녹아야함..
-    # 따라서 각 빙하에 접한 물의 수를 알아야함.
-    
-    # point1. bfs 을 통해서 빙하 그룹 수 파악
-    # point2. 빙하 위치 파악과 물의 영향도 파악
     
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
     
     def in_range(r, c):
-        return 0 <= r < n and 0 <= c < m
+        return 0 <= r < n and 0 <= c and m
     
-    def find_ice():
-        ices = []
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] != 0:
-                    ices.append((i, j))
-        return ices
-    
-    def bfs(ices):
+    def find_ice(): # 빙하 판단
         
-        queue = [[False for _ in range(m)] for _ in range(n)]
+        in_queue = [[False for _ in range(m)] for _ in range(n)]
         
-        cnts = []
-        
-        for ice in ices:
-            r, c = ice
-            
-            if queue[r][c] or board[r][c] == 0: # 못도는거 체크
-                continue
-            
+        def bfs(r, c):
             dq = deque([])
             
             dq.append((r, c))
-            queue[r][c] = True
-            cnt = 1
+            in_queue[r][c] = True
+            ice = [(r, c)]
             
             while dq:
                 cr, cc = dq.popleft()
                 for k in range(4):
                     nr = cr + dr[k]
                     nc = cc + dc[k]
-                    if  in_range(nr, nc) and\
-                        board[nr][nc] != 0 and\
-                        not queue[nr][nc]:
-                            dq.append((nr, nc))
-                            queue[nr][nc] = True
-                            cnt += 1
-            cnts.append(cnt)
-            
-        return len(cnts) # cnts 1개 - 정상 / 2개 - 나눠짐 / 0개 - 다 녹음
+                    if in_range(nr, nc) and\
+                        not in_queue[nr][nc] and\
+                            board[nr][nc] != 0:
+                                dq.append((nr, nc))
+                                in_queue[nr][nc] = True
+                                ice.append((nr, nc))
+            return ice
         
-
-    def water_effect(ices):
-        waters = [[0 for _ in range(m)] for _ in range(n)]
-        
-        for ice in ices:
-            cr, cc = ice
-            for k in range(4):
-                nr = cr + dr[k]
-                nc = cc + dc[k]
-                if in_range(nr, nc) and board[nr][nc] == 0:
-                    waters[cr][cc] -= 1
-                    
+        ices = defaultdict(list)
+        cnt = 1
         for i in range(n):
             for j in range(m):
-                board[i][j] = max(0, board[i][j] + waters[i][j])
-                
+                if in_queue[i][j] or board[i][j] == 0:
+                    continue
+                ice = bfs(i, j)
+                ices[cnt] = ice
+                cnt += 1
     
-    t = 0
+        return ices
+
+    def melt(ices): 
+        # ices = find_ice().values()
+        check_water = []
+        for ice in ices:
+            for r, c in ice:
+                cnt = 0
+                for k in range(4):
+                    nr = r + dr[k]
+                    nc = c + dc[k]
+                    if in_range(nr, nc) and\
+                        board[nr][nc] == 0:
+                            cnt += 1
+                check_water.append(cnt)
+        
+        # print(ices)
+        # print(check_water)
+        
+        for ice in ices:
+            for (r, c), cnt in zip(ice, check_water):
+                board[r][c] = max(board[r][c] - cnt, 0)
+
+    
+    time = 0
     while True:
-        ices = find_ice()
-        # print("영향 받기 전")
-        # for row in board:
-        #     print(row)
-        water_effect(ices) 
-        # print("영향 받기 후")
-        # for row in board:
-        #     print(row)
-        t += 1 
-        cnt = bfs(ices)
-        # print("그룹 수", cnt)
-        if cnt == 0:
+        ices = find_ice()   # {
+                            #    1 : [(r1, c1)...]
+                            #  , 2 : [(r2, c2)..]]
+                            # }
+                            
+        if len(ices.keys()) == 0:
             return 0
-        if cnt >= 2:
-            return t
+        
+        if len(ices.keys()) >= 2:
+            return time
+        
+        time += 1
+        melt(ices.values())
+    
 
 print(solution(n, m, board))
