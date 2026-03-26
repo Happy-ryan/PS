b612610347553d591ef8d2d5d4a405e1a8126a61
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-27 03:01:28 +0900

    [Platinum V] Title: 도로포장, Time: 2124 ms, Memory: 214048 KB -BaekjoonHub

--- 백준/Platinum/1162. 도로포장/도로포장.py
+++ 백준/Platinum/1162. 도로포장/도로포장.py
@@ -1,47 +1,55 @@
-from heapq import heappush, heappop
-
 n, m, k = map(int, input().split())
+edges = [list(map(int, input().split())) for _ in range(m)]
 
-adj = [[] for _ in range(n + 1)]
-for _ in range(m):
-    a, b, cost = map(int, input().split())
-    adj[a].append((cost, b))
-    adj[b].append((cost, a))
-
-
-def solution(n, m, k, adj):
+def solution(n, m, k, edges):
     
-    def dijkstra(start):
+    adj = [[] for _ in range(n + 1)]
+    for u, v, t in edges:
+        adj[u].append((t, v))
+        adj[v].append((t, u))
         
-        inf = int(1e18)
-        # k - 포장도로의 수를 의미한다!
-        visited = [[inf for _ in range(k + 1)] for _ in range(n + 1)]
+    # 도로 포장 / 포장 아니냐 > 벽 부수기 처럼 차원 하나 더 추가
+    inf = int(1e18)
+    dist = [[inf for _ in range(n + 1)] for _ in range(k + 1)]
 
+    def dijkstra(s, e):
+    
+        from heapq import heappop, heappush
         
         heap = []
-        # (비용, 지점, 포장도로 개수)
-        heappush(heap, (0, start, 0))
-        visited[start][0] = 0
+        
+        dist[0][1] = 0
+        heappush(heap, (0, 0, 1)) # 포장, 비용, 노드
         
         while heap:
-            d, cur, ck = heappop(heap)
+            ck, cd, cur = heappop(heap)
             
-            if d > visited[cur][ck]:
+            if dist[ck][cur] < cd: # 갈 곳에 기록된 비용 < 현재 들어갈 비용 : 이동 손해
                 continue
             
-            for nd, nxt in adj[cur]:
-                # 포장안했어...
-                if visited[nxt][ck] > d + nd:
-                    visited[nxt][ck] = d + nd
-                    heappush(heap, (visited[nxt][ck], nxt, ck))
-                # 포장을 했어..
-                nk = ck + 1
-                if nk < k + 1 and visited[nxt][nk] > d + 0:
-                    visited[nxt][nk] = d + 0
-                    heappush(heap, (visited[nxt][nk], nxt, nk))
+            for cost, nxt in adj[cur]:
+                nd = cost + cd
+                if dist[ck][nxt] > nd:
+                    dist[ck][nxt] = nd
+                    heappush(heap, (ck, nd, nxt))
+            
+                nk = ck + 1 # 포장함 > 비용 0 무조건 이동 이득!
                 
-        return visited
+                if nk > k: # 없는 차원
+                    continue
+                
+                if dist[nk][nxt] > 0 + cd:
+                    dist[nk][nxt] = 0 + cd
+                    heappush(heap, (nk, dist[nk][nxt], nxt))
+
+                    
+            
+    dijkstra(1, n)
     
-    return min(dijkstra(1)[n])
-    
-print(solution(n, m, k, adj))
+    min_ans = inf
+    for row in dist:
+        min_ans = min(min_ans, row[n])
+        
+    return min_ans
+        
+print(solution(n, m, k, edges))
