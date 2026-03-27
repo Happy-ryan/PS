11f77bb2c72630a8afae345a687167c2b8668ff9
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-28 06:46:06 +0900

    [Gold III] Title: 중량제한, Time: 332 ms, Memory: 128420 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,52 +1,47 @@
-N, M = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(M)]
-X, Y = map(int, input().split())
+n, m = map(int, input().split())
+edges = [list(map(int, input().split())) for _ in range(m)]
+A, B = map(int, input().split())
 
-def solution1(N, M, edges, X, Y):
+def solution(n, m, edges, A, B):
     
-    edges.sort(key=lambda x : -x[2])
-    
-    inf = int(1e10)
-    par = [-1] * (N + 1)
-    weights = [inf] * (N + 1)
-    
+    # union find
+    inf = int(1e18)
+    par = [-1 for _ in range(n + 1)]
+    weight = [inf for _ in range(n + 1)] # 다리 옮길 수 있는 최대 무게 중 최소 무게..
+    # root 뱉어내는 함수
     def find(x):
+        # x가 root -> 끝
         if par[x] == -1:
-            return x    
-        
-        root = find(par[x])
-        par[x] = root
-        return root
-    
+            return x
+        # x 가 root 아님 -> par[x]의 root 필요
+        par[x] = find(par[x])
+        return par[x]
+    # y -> x (root에 가깝도록) - 작은 놈이 root에 가깝도록
     def union(x, y, w):
         x, y = find(x), find(y)
-        
         if x == y:
             return False
         
+        if x > y: x, y = y, x
         par[y] = x
-        weights[x] = min(weights[x], w)
-        
+        weight[x] = min(weight[x], w) # 최대 중량 중 최소
         return True
-    
-    for edge in edges:
-        A, B, W = edge
-        # print(f"A: {A}, B: {B}, W: {W}")
-        # print(f"결합전, par: {par[1:]} / weights: {weights[1:]}")
-        union(A, B, W)
-        # print(f"결합후, par: {par[1:]} / weights: {weights[1:]}")
-        # print("-")
         
-        # W를 정렬해놔서 A, B가 연결되면 더 이상 합칠 필요 없음.
-        # A, B 연결되면 그 이후는 더 이상 금지.
-        if find(X) == find(Y):
-            # print("끝 by 임짱")
+    
+    # union - 중량이 큰 것(최대)부터 결합하고 그 중 최소 
+    edges.sort(key=lambda x : -x[2])
+    for u, v, w in edges:
+        # print(f"결합 전: {par}")
+        union(u, v, w) # 결합순서가 영향을 줌..> 정렬이 필요함!!!
+        # print(f"결합 후: {par}")
+        # print('-')
+        # 결합 완료 > 종료를 안시키면 최대 무게가 아니라 최소 무게로 갱신될 것.
+        if find(A) == find(B):
             break
         
-    # print("루트",find(X), find(Y))
-    # print(par)
-    # print(weights)
     
-    return weights[find(A)]
+    return weight[find(B)]
+        
     
-print(solution1(N, M, edges, X, Y))
+        
+print(solution(n, m, edges, A, B))
