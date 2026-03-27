747bb2b1598724c318efb100d579a909a24ea942
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-27 13:47:56 +0900

    [Gold III] Title: 파티, Time: 1064 ms, Memory: 121948 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,27 +1,26 @@
-N, M, X = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(M)]
+n, m, x = map(int, input().split())
+edges = [list(map(int, input().split())) for _ in range(m)]
 
-from heapq import heappush, heappop
-
-def solution(N, M, X, edges):
-    # 단방향 / 오고가는가는데 가장 오래 걸리는 학생
-    
-    adj = [[] for _ in range(N + 1)]
-    radj = [[] for _ in range(N + 1)]
-    
-    for u, v, t in edges:
-        adj[u].append((t, v)) # 최단거리로 가야하기때문에 최소 시간 구해야함.
-        radj[v].append((t, u)) # 양방향처럼 되어버렸음.. <- 이거 쓰면 안돼!
+def solution(n, m, x, edges):
+    # 단방향
+    adj = [[] for _ in range(n + 1)]
+    # 역방향도 필요
+    # radj = [[] for _ in range(n + 1)]
+    for u, v, c in edges:
+        adj[u].append((c, v)) # 비용, 노드
+    #   radj[v].append((c, u)) <= 양방향처럼 작용함.
     
     inf = int(1e18)
     
-    
-    def dijkstra(start, adj, dist):
+    def dijkstra(s, e, adj):
+        
+        from heapq import heappop, heappush
         
         heap = []
+        dist = [inf for _ in range(n + 1)]
         
-        dist[start] = 0
-        heappush(heap, (0, start))
+        dist[s] = 0
+        heappush(heap, (0, s)) # 비용, 노드
         
         while heap:
             cd, cur = heappop(heap)
@@ -29,23 +28,25 @@ def solution(N, M, X, edges):
             if dist[cur] < cd:
                 continue
             
-            for nd, nxt in adj[cur]:
-                if dist[nxt] > cd + nd:
-                    dist[nxt] = cd + nd
-                    heappush(heap, (dist[nxt], nxt))
-    # (rdist)시작점1개 + 도착점 여러개 <-> (dist)현 상황 : 시작점 여러개 + 도착점 1개 (도착점 기준 생각)
-    # 집에서 파티장 갈 때 : 도착점1개에서 -> 여러집(시작점)으로 가는 최소 비용 
-    # 파티절에서 집 갈 때 : 시작점1개 -> 여러집(도착점)
-    answer = 0
-    dist = [inf for _ in range(N + 1)]
-    dijkstra(X, adj, dist) 
-    rdist = [inf for _ in range(N + 1)]
-    dijkstra(X, radj, rdist)
+            for cost, nxt in adj[cur]:
+                nd = cd + cost
+                if dist[nxt] > nd:
+                    dist[nxt] = nd
+                    heappush(heap, (nd, nxt))
+        
+        return dist
     
-    res = -inf
-    for i in range(1, N + 1):
-        res = max(res, dist[i] + rdist[i])
+    answer = -inf
     
-    return res
+    for i in range(1, n + 1):
+        dist = dijkstra(i, x, adj)
+        rdist = dijkstra(x, i, adj)
+        # print(dist)
+        # print(rdist)
+        answer = max(answer, dist[x] + rdist[i])
+        # print(dist[x] + rdist[i])
+        # print("-")
+    
+    return answer
 
-print(solution(N, M, X, edges))
+print(solution(n, m, x, edges))
