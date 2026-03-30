173929e4d68a2d0dec3462f79582a7ef88f0c010
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2026-03-30 20:21:39 +0900

    [Gold III] Title: 앱, Time: 120 ms, Memory: 115208 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,32 +1,38 @@
-n, m = map(int, input().split())
-memories = list(map(int, input().split()))
+N, M = map(int, input().split())
+memorys = list(map(int, input().split()))
 costs = list(map(int, input().split()))
 
-def solution(n, m, memories, costs):
-    # m = 10,000,000 / max_cost = 100 * 100
-    # dp[i][j] i번째 앱(item)을 선택하고 j비용(weight) 일 때, 최대 메모리(value)의 값
-    # 특정 비용일 때, 메모리를 최대한 비활성화 시키고 싶다!
-    memories = [0] + memories
-    costs = [0] + costs
-    inf = int(1e9)
-    max_cost = 100 * 100
-    dp = [[-inf for _ in range(max_cost + 1)] for _ in range(n + 1)]
+def solution(N, M, memorys, costs):
+    # M 이라는 메모리를 확보하기 위한 최소 비용
+    # 아이템 - 비용(보통)
+    # 행 - i번째 앱 선택
+    # 열 - 메모리(보통)
     
+    # 메모리 열...10,000,000 * 100 <- 너무 크다
+    # 비용 열 ... 100 * 100
+    # 비용이 c일 때 확보된 메모리(M) 
+    
+    # 아이템 - 메모리 / 열 - 비용
+    inf = int(1e18)
+    col = 100 * 100 + 1
+    # inf : 메모리..최소비용으로 큰 메모리 넣어야 좋은 것,
+    dp = [[-inf for _ in range(col)] for _ in range(N + 1)]
+    
+    # 기초
     dp[0][0] = 0
-    for i in range(1, n + 1):
-        w, v = costs[i], memories[i]
-        for j in range(max_cost, -1, -1):
-            dp[i][j] = dp[i - 1][j]
-            if j - w >= 0:
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + v)
+    memorys = [0] + memorys # 0base -> 1base
+    costs = [0] + costs # 0base -> 1base
+    # dp[i][j] = [max, min](dp[i][j], dp[i - 1][j - w] + v)
+    for i in range(1, N + 1):
+            c, m = costs[i], memorys[i]
+            for j in range(col):
+                dp[i][j] = dp[i - 1][j]
+                if j - c >= 0:
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - c] + m)
     
-    min_cost = inf
-    # cost의 범위는 0 ~ 100 * 100d이다!!!!!!!!
-    for cost in range(max_cost + 1):
-        if dp[n][cost] >= m:
-            min_cost = min(min_cost, cost)
-            
-    return min_cost
-
-
-print(solution(n, m, memories, costs))
+    # 비용 0부터 순회하며 처음으로 메모리 M을 넘기는 비용 찾기
+    for cost in range(col + 1):
+        if dp[N][cost] >= M:
+            return cost
+        
+print(solution(N, M, memorys, costs))
